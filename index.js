773b7bfd5362a98f6f37d89df3188356ccd27d27
Patrick Mandl <mandl@e-conomix.at>
2021-02-22 09:14:57 +0100

    :construction: Try to get hydrate content

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const component = build.compile('./src/App.svelte');
 const rendered = build.render(component, { name: 'P@', details: true });
 console.log('rendered');
 console.log(rendered.result.html)
+console.log(rendered.hydrate)
 
 var hr_end = process.hrtime(hr_start); // hr_end[0] is in seconds, hr_end[1] is in nanoseconds
 const timeInMs = (hr_end[0]* 1000000000 + hr_end[1]) / 1000000; // convert first to ns then to ms

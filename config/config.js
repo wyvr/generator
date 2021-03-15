da1f3bd0dfd5e5df92495c615d71cd159939477e
Wyvr <wyvr@daviot.at>
2021-03-15 09:10:05 +0100

    :zap: Add performance measuring in the import

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@ module.exports = {
     },
     import: {
         format_processed_file: true, // boolean: whether the output of the imported and processed file should be prettyprinted or not
+        measure_performance: true, // boolean: whether the import process should be performance measured
     },
 };

e52015eaba1ff70b63094df4aa91ab04d1fa23d1
wyvr <wyvr@daviot.at>
2021-05-03 08:59:05 +0200

    :white_check_mark: add tests

--- test/lib/dir.test.js
+++ test/lib/dir.test.js
@@ -1,7 +1,42 @@
 const assert = require('assert');
 const dir = require('./../../lib/dir');
+const fs = require('fs-extra');
+const { v4: uuidv4, v4 } = require('uuid');
+const path = require('path');
 
 describe('Lib/Dir', () => {
     describe('create', () => {
-    })
+        it('single', () => {
+            const name = uuidv4().split('-')[0];
+            assert.strictEqual(fs.existsSync(name), false);
+            dir.create(name);
+            assert.strictEqual(fs.existsSync(name), true);
+            fs.removeSync(name)
+        });
+        it('deep', () => {
+            const name = path.join(...uuidv4().split('-'));
+            assert.strictEqual(fs.existsSync(name), false);
+            dir.create(name);
+            assert.strictEqual(fs.existsSync(name), true);
+            fs.removeSync(name)
+        });
+    });
+    describe('delete', () => {
+        it('single', () => {
+            const name = uuidv4().split('-')[0];
+            dir.create(name);
+            assert.strictEqual(fs.existsSync(name), true);
+            dir.delete(name);
+            assert.strictEqual(fs.existsSync(name), false);
+            fs.removeSync(name)
+        });
+        it('deep', () => {
+            const name = path.join(...uuidv4().split('-'));
+            dir.create(name);
+            assert.strictEqual(fs.existsSync(name), true);
+            dir.delete(name);
+            assert.strictEqual(fs.existsSync(name), false);
+            fs.removeSync(name)
+        });
+    });
 });

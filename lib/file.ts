179d3dc42b0f477f357b4ddc6c298acdb9983c9b
wyvr <wyvr@daviot.at>
2021-05-18 18:12:45 +0200

    :sparkles: read file as json

--- lib/file.ts
+++ lib/file.ts
@@ -5,11 +5,11 @@ import { dirname } from 'path';
 export class File {
     /**
      * converts the given filename to the filename with the given extension
-     * @param filename 
-     * @param extension 
+     * @param filename
+     * @param extension
      * @returns filename with the given extension
      */
-    static to_extension(filename:string, extension:string): string {
+    static to_extension(filename: string, extension: string): string {
         if (!filename || typeof filename != 'string' || !extension || typeof extension != 'string') {
             return '';
         }
@@ -25,19 +25,19 @@ export class File {
         }
         return [...splitted, extension].join('.');
     }
-    // 
+    //
     /**
      * create the directory to contain a specific file
-     * @param filename 
+     * @param filename
      */
-    static create_dir(filename: string):void {
+    static create_dir(filename: string): void {
         const dir_path = dirname(filename);
         fs.mkdirSync(dir_path, { recursive: true });
     }
     /**
      * adds the path part index.html to the filename when it is a folder
-     * @param {string} filename
-     * @param {string} extension
+     * @param filename
+     * @param extension
      * @returns filename
      */
     static to_index(filename: string, extension: string = null): string {
@@ -60,5 +60,24 @@ export class File {
             return parts.join('/');
         }
         return filename;
+    }
+    /**
+     * read the content of an file as json
+     * @param filename
+     * @returns the data of the file
+     */
+    static read_json(filename: string): any {
+        const content = fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
+        if (!content) {
+            return null;
+        }
+        let data = null;
+        try {
+            data = JSON.parse(content);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+        return data;
     }
 }

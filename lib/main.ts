9b5c33dd844d98ce452cca9792eb18bc912a26b1
Wyvr <wyvr@daviot.at>
2021-05-21 11:30:32 +0200

    :sparkles: add symlink for assets

--- lib/main.ts
+++ lib/main.ts
@@ -95,7 +95,7 @@ export class Main {
         // fs.writeFileSync('./pub/index.html', demo_file);
 
         // symlink the "static" folders to pub
-        // link.to_pub('assets');
+        Link.to_pub('assets');
 
         var hr_end = process.hrtime(hr_start); // hr_end[0] is in seconds, hr_end[1] is in nanoseconds
         const timeInMs = (hr_end[0] * 1000000000 + hr_end[1]) / 1000000; // convert first to ns then to ms

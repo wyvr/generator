857eb3b9dd99939edbfb05b3879a68fecf9f8d14
Wyvr <wyvr@daviot.at>
2021-05-25 22:51:00 +0200

    :bento: add new favicon and include global css

--- src/doc/Default.svelte
+++ src/doc/Default.svelte
@@ -6,6 +6,8 @@
 <html lang="{data._wyvr.language}">
     <head>
         <title>{data.title}</title>
+        <link rel="icon" href="/assets/favicon.png">
+        <link href="/assets/global.css" rel="stylesheet" />
     </head>
     <body>
         <code>&lt; Default Doc &gt;</code>

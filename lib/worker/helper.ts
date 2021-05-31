73c8a7dabde420c7c0c29724cbbbae555bbb37c1
wyvr <wyvr@daviot.at>
2021-05-31 12:41:40 +0200

    :loud_sound: add debug for worker actions

--- lib/worker/helper.ts
+++ lib/worker/helper.ts
@@ -14,15 +14,23 @@ export class WorkerHelper {
         this.send_action(WorkerAction.status, enum_status);
     }
     static send_action(action: WorkerAction, data) {
-        this.send({
+        const data_to_send = {
             action: {
                 key: action,
-                value: data
+                value: data,
+            },
+        };
+        // add human readable info when in debug mode
+        if (process.env.WYVR_ENV == 'debug') {
+            (<any>data_to_send).action.key_name = WorkerAction[action];
+            if (action == WorkerAction.status) {
+                (<any>data_to_send).action.value_name = WorkerStatus[data];
             }
-        });
+        }
+        this.send(data_to_send);
     }
     static get_status(status) {
-        let enum_status:any = WorkerStatus[status];
+        let enum_status: any = WorkerStatus[status];
         if (!enum_status) {
             enum_status = WorkerStatus.exists;
         }
@@ -34,7 +42,7 @@ export class WorkerHelper {
     static log(type: LogType, ...messages: any[]) {
         this.send_action(WorkerAction.log, {
             type,
-            messages
-        })
+            messages,
+        });
     }
 }

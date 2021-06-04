4ff373f9fc4a20da120ec8d9a039c21774306107
Wyvr <wyvr@daviot.at>
2021-06-04 13:08:29 +0200

    :clown-face: add static global dummy

--- src/test/Static.svelte
+++ src/test/Static.svelte
@@ -1,12 +1,15 @@
 <script>
     export let value = null;
+    export let with_global = false;
 </script>
 
 <code class:has_value={value}>
     {#if value}
         <pre>{JSON.stringify(value, null, 4)}</pre>
     {:else}
-        Static
+        Static {#if with_global}
+        global `faker.buzzwords[0]` "{getGlobal('faker.buzzwords[0]')}"
+        {/if}
     {/if}
 </code>
 

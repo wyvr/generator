a5cb8b54434226341a9f84f985f3bba46027e84f
Wyvr <wyvr@daviot.at>
2021-06-04 13:09:25 +0200

    :clown_face: add gloabl static example and more todos

--- src/page/Test.svelte
+++ src/page/Test.svelte
@@ -8,15 +8,22 @@
 {#if data.title}
     <h1>{data.title}</h1>
 {/if}
+<slot />
 <section>
     <ul>
         <li>Static &raquo;<Static />&laquo;</li>
         <li>Static with prop `data.product`&raquo;<Static value={data.product} />&laquo;</li>
+        <li>Static with global &raquo;<Static with_global={true} />&laquo;</li>
         <li>Hydrate Inline &raquo;<HydrateInline />&laquo;</li>
         <li>Hydrate Block &raquo;<HydrateBlock />&laquo;</li>
     </ul>
+    <h2>TODOs</h2>
+    <ul>
+        <li>Hydrate with value from global</li>
+        <li>Hydrate with value from static parent</li>
+        <li>Hydrate with slot</li>
+    </ul>
 </section>
-<slot />
 
 <style>
     h1 {

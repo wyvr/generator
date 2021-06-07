b7377d5396ffd27acd016a5f9aaca62afde5fc0f
wyvr <wyvr@daviot.at>
2021-06-07 09:12:00 +0200

    :wrench: add dummy entry in the config

--- config/config.js
+++ config/config.js
@@ -6,4 +6,5 @@ module.exports = {
         format_processed_file: true, // boolean: whether the output of the imported and processed file should be prettyprinted or not
         measure_performance: true, // boolean: whether the import process should be performance measured
     },
+    themes: null // list of the themes which should be used
 };

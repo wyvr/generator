b86e7e507bdb208fa82f9daa1b58944fddff94f3
wyvr <wyvr@daviot.at>
2021-06-11 12:54:54 +0200

    :memo: update default config

--- config/config.js
+++ config/config.js
@@ -6,5 +6,6 @@ module.exports = {
         format_processed_file: true, // boolean: whether the output of the imported and processed file should be prettyprinted or not
         measure_performance: true, // boolean: whether the import process should be performance measured
     },
-    themes: null // list of the themes which should be used
+    themes: null, // list of the themes which should be used
+    assets: [], // list of objects { src: 'path_to_source', target: 'relative_path_in_assets' }, to copy files into the assets folder
 };

8df3a2dfbcc17435e422359e437aa57bade50ca9
Wyvr <wyvr@daviot.at>
2021-06-18 13:51:17 +0200

    :sparkles: add svelte sources to routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
-import { join, dirname } from 'path';
+import { join, dirname, resolve } from 'path';
 import { File } from '@lib/file';
+import { Build } from '@lib/build';
+import { Client } from '@lib/client';
+import { Logger } from './logger';
 
 export class Routes {
     static collect_routes(dir: string = null) {
@@ -19,7 +22,7 @@ export class Routes {
                 result.push(...this.collect_routes(path));
                 return;
             }
-            if (stat.isFile() && entry.match(/\.js$/)) {
+            if (stat.isFile() && entry.match(/\.js|\.svelte$/)) {
                 result.push(path);
             }
         });
@@ -28,14 +31,61 @@ export class Routes {
     static async execute_routes(routes: string[]) {
         const result = await Promise.all(
             routes.map(async (route) => {
-                const route_module = await require(route);
-                if (!Array.isArray(route_module)) {
-                    return [route_module];
+                if (route.match(/\.js$/)) {
+                    const route_module = await require(route);
+                    return this.inject_source(route_module);
                 }
-                return route_module;
+                // if (route.match(/\.svelte$/)) {
+                //     // convert svelte files to json
+                //     const url = route.replace(join(process.cwd(), 'gen/routes'), '').replace(/\.svelte$/, '');
+                //     const content = fs.readFileSync(route, { encoding: 'utf-8' });
+                //     const config = Client.parse_wyvr_config(content);
+                //     console.log(config);
+                //     // const match = content.match(/wyvr:\s+(\{[^}]+\})/);
+                //     // if (match) {
+                //     //     console.log(match)
+                //     // }
+                //     const compiled = Build.compile(content);
+                //     const rendered = Build.render(compiled, {});
+                //     return {
+                //         url,
+                //         content: `${rendered.result.html}<style>${rendered.result.css.code}</style>`,
+                //     };
+                // }
+                return null;
             })
         );
-        return [].concat(...result);
+        return [].concat(...result.filter((x) => x));
+    }
+    static inject_source(route_entries: any[] | any) {
+        if (!Array.isArray(route_entries)) {
+            route_entries = [route_entries];
+        }
+        return route_entries.map((entry: any) => {
+            // ignore entries without sources
+            if (!entry?._wyvr?.source) {
+                return entry;
+            }
+            const source_path = join(process.cwd(), 'gen/routes', entry._wyvr.source);
+            if (!fs.existsSync(source_path)) {
+                entry.error = `source file ${source_path} does not exist`;
+                return entry;
+            }
+            const source_code = fs.readFileSync(source_path, { encoding: 'utf-8' });
+            if (!source_code) {
+                entry.error = `empty source file ${source_path}`;
+                return entry;
+            }
+            const compiled = Build.compile(source_code);
+            if (compiled.error) {
+                // svelte error messages
+                Logger.error('[svelte]', source_path, compiled);
+                return entry;
+            }
+            const rendered = Build.render(compiled, entry);
+            entry.content = `${rendered.result.html}<style>${rendered.result.css.code}</style>`;
+            return entry;
+        });
     }
     static write_routes(route_entries: any[], hook_before_process: Function = null) {
         return route_entries.map((data, index) => {

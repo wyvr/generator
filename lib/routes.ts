0b73641bd2483837264400d085487b24a464aff0
Wyvr <wyvr@daviot.at>
2021-06-21 16:14:51 +0200

    :fire: remove abilitry that routes can have own sources

--- lib/routes.ts
+++ lib/routes.ts
@@ -29,93 +29,15 @@ export class Routes {
         });
         return result;
     }
-    // @obsolete
-    static async execute_routes(routes: string[]) {
-        const result = await Promise.all(
-            routes.map(async (route) => {
-                if (route.match(/\.js$/)) {
-                    const route_module = await require(route);
-                    return this.inject_source(route_module);
-                }
-                // if (route.match(/\.svelte$/)) {
-                //     // convert svelte files to json
-                //     const url = route.replace(join(process.cwd(), 'gen/routes'), '').replace(/\.svelte$/, '');
-                //     const content = fs.readFileSync(route, { encoding: 'utf-8' });
-                //     const config = Client.parse_wyvr_config(content);
-                //     console.log(config);
-                //     // const match = content.match(/wyvr:\s+(\{[^}]+\})/);
-                //     // if (match) {
-                //     //     console.log(match)
-                //     // }
-                //     const compiled = Build.compile(content);
-                //     const rendered = Build.render(compiled, {});
-                //     return {
-                //         url,
-                //         content: `${rendered.result.html}<style>${rendered.result.css.code}</style>`,
-                //     };
-                // }
-                return null;
-            })
-        );
-        return [].concat(...result.filter((x) => x));
-    }
     static async execute_route(route: string) {
         if (route.match(/\.js$/)) {
             const route_module = await require(route);
             if (!Array.isArray(route_module)) {
-                return this.inject_source([route_module]);
+                return [route_module];
             }
-            return this.inject_source(route_module);
+            return route_module;
         }
-        // if (route.match(/\.svelte$/)) {
-        //     // convert svelte files to json
-        //     const url = route.replace(join(process.cwd(), 'gen/routes'), '').replace(/\.svelte$/, '');
-        //     const content = fs.readFileSync(route, { encoding: 'utf-8' });
-        //     const config = Client.parse_wyvr_config(content);
-        //     console.log(config);
-        //     // const match = content.match(/wyvr:\s+(\{[^}]+\})/);
-        //     // if (match) {
-        //     //     console.log(match)
-        //     // }
-        //     const compiled = Build.compile(content);
-        //     const rendered = Build.render(compiled, {});
-        //     return {
-        //         url,
-        //         content: `${rendered.result.html}<style>${rendered.result.css.code}</style>`,
-        //     };
-        // }
         return null;
-    }
-    static inject_source(route_entries: any[] | any) {
-        if (!Array.isArray(route_entries)) {
-            route_entries = [route_entries];
-        }
-        return route_entries.map((entry: any) => {
-            // ignore entries without sources
-            if (!entry?._wyvr?.source) {
-                return entry;
-            }
-            const source_path = join(process.cwd(), 'gen/routes', entry._wyvr.source);
-            if (!fs.existsSync(source_path)) {
-                entry.error = `source file ${source_path} does not exist`;
-                return entry;
-            }
-            Client.correct_svelte_file_import_paths([new WyvrFile(source_path)]);
-            const source_code = fs.readFileSync(source_path, { encoding: 'utf-8' });
-            if (!source_code) {
-                entry.error = `empty source file ${source_path}`;
-                return entry;
-            }
-            const compiled = Build.compile(source_code);
-            if (compiled.error) {
-                // svelte error messages
-                Logger.error('[svelte]', source_path, compiled);
-                return entry;
-            }
-            const rendered = Build.render(compiled, entry);
-            entry.content = `${rendered.result.html}<style>${rendered.result.css.code}</style>`;
-            return entry;
-        });
     }
     static write_routes(route_entries: any[], hook_before_process: Function = null) {
         if (!route_entries) {

503c02f2480a312eb1a95bac78807846cc2c85c3
Wyvr <wyvr@daviot.at>
2021-07-02 07:13:39 +0200

    :sparkles: improve error output and test it

--- lib/error.ts
+++ lib/error.ts
@@ -1,18 +1,69 @@
 import { Logger } from '@lib/logger';
+import { join } from 'path';
 
 export class Error {
-    static get(e: any, filename: string, scope: string = null): string {
-        if (e.message) {
-            const stack = e.stack.split('\n');
-            if (stack) {
-                e.code = stack[0].slice(0, stack[0].indexOf(':'));
-                e.requireStack = stack.slice(1).map((stack_entry) => {
-                    return stack_entry.slice(stack_entry.indexOf('at') + 3);
-                });
+    static extract(e: any, source: string) {
+        const root_dir = process.cwd();
+        const object = {
+            code: null,
+            filename: null,
+            hint: null,
+            message: null,
+            name: e.name || null,
+            source: null,
+            stack: null,
+        };
+        if (source) {
+            object.source = source.replace(root_dir + '/', '');
+        }
+        const gen_dir = join(root_dir, 'gen');
+        let stack = null;
+        if (e.stack) {
+            stack = e.stack.split('\n');
+        }
+        if (stack) {
+            object.stack = stack
+                .map((entry) => {
+                    // when line starts with "- <cwd>/gen"
+                    const match = entry.match(/^- (.*?\/gen\/.*)$/);
+                    if (match) {
+                        return match[1].replace(gen_dir + '/', '');
+                    }
+                    // when line contains " at /" and the "<cwd>/gen"
+                    if (entry.indexOf(gen_dir) > -1 && entry.indexOf(' at /') > -1) {
+                        return entry.replace(/.*?at /, '').replace(gen_dir + '/', '');
+                    }
+
+                    return null;
+                })
+                .filter((x) => x);
+            // svelte errors
+            if (object.stack.length == 0 && stack[3] && stack[3].trim() == '') {
+                object.stack = [stack[0].trim().replace(gen_dir + '/', '')];
+                object.hint = stack.slice(1, 3).join('\n');
             }
         }
-        return `${Logger.color.bold('@' + scope ?? '')}\n[${Logger.color.bold(e.code)}] ${e.message ?? '-'}\n${Logger.color.dim('in')} ${e.requireStack ? e.requireStack[0] : '-'}\n${Logger.color.dim(
-            'source file'
-        )} ${filename}`;
+        if (e.message) {
+            object.message = e.message.split('\n')[0];
+        }
+
+        return object;
+    }
+    static get(e: any, filename: string, scope: string = null): string {
+        const data = this.extract(e, filename);
+        const result = [];
+        if (scope) {
+            result.push(Logger.color.bold('@' + scope));
+        }
+        result.push(`[${Logger.color.bold(data.name)}] ${data.message ?? '-'}`);
+        if (data.stack) {
+            result.push(Logger.color.dim('stack'));
+            result.push(...data.stack.map((entry) => `${Logger.color.dim('-')} ${entry}`));
+        }
+        if (data.source) {
+            result.push(`${Logger.color.dim('source')} ${data.source}`);
+        }
+
+        return result.join('\n');
     }
 }
---
+++ test/lib/error.test.js
@@ -0,0 +1,258 @@
+const { join } = require('path');
+
+require('module-alias/register');
+
+describe('Lib/Error', () => {
+    const assert = require('assert');
+    const { Error } = require('@lib/error');
+    const cwd = process.cwd();
+
+    describe('extract', () => {
+        it('structure', () => {
+            const result = Error.extract(new SyntaxError('hi'));
+            assert.deepStrictEqual(Object.keys(result), ['code', 'filename', 'hint', 'message', 'name', 'source', 'stack']);
+        });
+        it('JS error', () => {
+            const error = {
+                name: 'TypeError',
+                message: 'The "path" argument must be of type string or an instance of Buffer or URL. Received null',
+                stack: `TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string or an instance of Buffer or URL. Received null
+                    at Object.openSync (fs.js:453:10)
+                    at Object.readFileSync (fs.js:364:35)
+                    at ${cwd}/gen/plugins/wyvr/example/index.js:26:36
+                    at Array.forEach (<anonymous>)
+                    at Object.after [as fn] (${cwd}/gen/plugins/wyvr/example/index.js:25:19)
+                    at Function.<anonymous> (${cwd}/wyvr/plugin.js:204:85)
+                    at step (${cwd}/wyvr/plugin.js:33:23)
+                    at Object.next (${cwd}/wyvr/plugin.js:14:53)
+                    at ${cwd}/wyvr/plugin.js:8:71
+                    at new Promise (<anonymous>)`,
+            };
+            assert.deepStrictEqual(Error.extract(error), {
+                code: null,
+                filename: null,
+                hint: null,
+
+                message: 'The "path" argument must be of type string or an instance of Buffer or URL. Received null',
+                name: 'TypeError',
+                source: null,
+                stack: [`plugins/wyvr/example/index.js:26:36`],
+            });
+        });
+        it('svelte error', () => {
+            const error = {
+                name: 'SyntaxError',
+                message: 'Cannot use import statement outside a module',
+                stack: `${cwd}/gen/src/test/store.js:1
+                    import { writable } from 'svelte/store';
+                    ^^^^^^
+                    
+                    SyntaxError: Cannot use import statement outside a module
+                        at wrapSafe (internal/modules/cjs/loader.js:1116:16)
+                        at Module._compile (internal/modules/cjs/loader.js:1164:27)
+                        at Object.Module._extensions..js (internal/modules/cjs/loader.js:1220:10)
+                        at Module.load (internal/modules/cjs/loader.js:1049:32)
+                        at Function.Module._load (internal/modules/cjs/loader.js:937:14)
+                        at Module.require (internal/modules/cjs/loader.js:1089:19)
+                        at require (internal/modules/cjs/helpers.js:73:18)
+                        at Object.<anonymous> (${cwd}/gen/src/test/StaticStore.svelte:5:19)
+                        at Module._compile (internal/modules/cjs/loader.js:1200:30)
+                        at Object.require.extensions.<computed> [as .svelte] (${cwd}/node_modules/svelte/register.js:49:17)`,
+            };
+            assert.deepStrictEqual(Error.extract(error), {
+                code: null,
+                filename: null,
+                hint: `                    import { writable } from 'svelte/store';
+                    ^^^^^^`,
+                message: 'Cannot use import statement outside a module',
+                name: 'SyntaxError',
+                source: null,
+                stack: [`src/test/store.js:1`],
+            });
+        });
+        it('source', () => {
+            const error = {
+                name: 'SyntaxError',
+                message: 'Cannot use import statement outside a module',
+            };
+            assert.deepStrictEqual(Error.extract(error, join(cwd, 'gen', 'src')), {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'Cannot use import statement outside a module',
+                name: 'SyntaxError',
+                source: 'gen/src',
+                stack: null,
+            });
+        });
+        it('stack', () => {
+            const error = {
+                name: 'SyntaxError',
+                stack: `test
+- ${cwd}/gen/src/test/minus
+                at ${cwd}/gen/src/test/at`,
+            };
+            assert.deepStrictEqual(Error.extract(error), {
+                code: null,
+                filename: null,
+                hint: null,
+                message: null,
+                name: 'SyntaxError',
+                source: null,
+                stack: ['src/test/minus', 'src/test/at'],
+            });
+        });
+        it('RangeError', () => {
+            let error = null;
+            try {
+                let list = Array(Number.MAX_VALUE);
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'Invalid array length',
+                name: 'RangeError',
+                source: null,
+                stack: [],
+            });
+        });
+        it('ReferenceError', () => {
+            let error = null;
+            try {
+                var a = a + b;
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'b is not defined',
+                name: 'ReferenceError',
+                source: null,
+                stack: [],
+            });
+        });
+        it('SyntaxError', () => {
+            let error = null;
+            try {
+                eval('a x b');
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'Unexpected identifier',
+                name: 'SyntaxError',
+                source: null,
+                stack: [],
+            });
+        });
+        it('TypeError', () => {
+            let error = null;
+            try {
+                let x = new 'String'();
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: '"String" is not a constructor',
+                name: 'TypeError',
+                source: null,
+                stack: [],
+            });
+        });
+        it('URIError', () => {
+            let error = null;
+            try {
+                decodeURI('%');
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'URI malformed',
+                name: 'URIError',
+                source: null,
+                stack: [],
+            });
+        });
+        it('Maximum call stack size exceeded', () => {
+            let error = null;
+            try {
+                function foo() {
+                    foo();
+                }
+                foo();
+            } catch (e) {
+                error = Error.extract(e);
+            }
+
+            assert.deepStrictEqual(error, {
+                code: null,
+                filename: null,
+                hint: null,
+                message: 'Maximum call stack size exceeded',
+                name: 'RangeError',
+                source: null,
+                stack: [],
+            });
+        });
+    });
+    describe('get', () => {
+        it('output', () => {
+            const error = {
+                name: 'SyntaxError',
+                message: 'Cannot use import statement outside a module',
+                stack: `test
+        - ${cwd}/gen/src/test/minus
+                at ${cwd}/gen/src/test/at`,
+            };
+            assert.deepStrictEqual(
+                Error.get(error),
+                `[\x1B[1mSyntaxError\x1B[22m] Cannot use import statement outside a module
+\x1B[2mstack\x1B[22m
+\x1B[2m-\x1B[22m src/test/at`
+            );
+        });
+        it('scoping', () => {
+            const error = {
+                name: 'SyntaxError',
+                stack: `test
+        - ${cwd}/gen/src/test/minus
+                at ${cwd}/gen/src/test/at`,
+            };
+            assert.deepStrictEqual(
+                Error.get(error, 'file', 'test'),
+`\x1B[1m@test\x1B[22m
+[\x1B[1mSyntaxError\x1B[22m] -
+\x1B[2mstack\x1B[22m
+\x1B[2m-\x1B[22m src/test/at
+\x1B[2msource\x1B[22m file`
+            );
+        });
+        it('minimal', () => {
+            const error = {
+            };
+            assert.deepStrictEqual(
+                Error.get(error, null, 'test'),
+`\x1B[1m@test\x1B[22m\n[] -`
+            );
+        });
+    });
+});

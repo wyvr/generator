5a872c97edb1de2306e15e00c15214ad0013f6bf
Wyvr <wyvr@daviot.at>
2021-07-02 13:01:50 +0200

    :white_check_mark: add test for require cache clearing

--- lib/require_cache.ts
+++ lib/require_cache.ts
@@ -1,15 +1,18 @@
 import { join } from 'path';
-import { Logger } from './logger';
+import { Logger } from '@lib/logger';
 
 export class RequireCache {
     static clear() {
         let amount = 0;
         Object.keys(require.cache).forEach((cache_file) => {
-            if (cache_file.indexOf(join(process.cwd(), 'gen')) > -1 || cache_file.indexOf(join(process.cwd(), 'imported')) > -1) {
+            if (this.matches(cache_file)) {
                 amount++;
                 delete require.cache[cache_file];
             }
         });
         Logger.debug('require cache cleared', amount, 'entries');
+    }
+    static matches(cache_file: string) {
+        return cache_file.indexOf(join(process.cwd(), 'gen')) > -1 || cache_file.indexOf(join(process.cwd(), 'imported')) > -1;
     }
 }
--- test/lib/require_cache.test.js
+++ test/lib/require_cache.test.js
@@ -1,10 +1,24 @@
+const { join } = require('path');
+
 require('module-alias/register');
 
 describe('Lib/RequireCache', () => {
     const assert = require('assert');
     const { RequireCache } = require('@lib/require_cache');
 
-    before(() => {
-        Env.set(EnvModel.dev);
+    before(() => {});
+    describe('clear', () => {
+        it('contains gen files', () => {
+            RequireCache.clear();
+            assert(Object.keys(require.cache).find((cache_file) => cache_file.indexOf('/gen/') > -1) == null);
+        });
+    });
+    describe('matches', () => {
+        it('gen', () => {
+            assert(RequireCache.matches(join(process.cwd(), 'gen/test/a')));
+        });
+        it('imported', () => {
+            assert(RequireCache.matches(join(process.cwd(), 'imported/data/a')));
+        });
     });
 });

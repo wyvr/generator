97c196df53d2d86c2bb14a391c4272442d5b5c45
Wyvr <wyvr@daviot.at>
2021-07-19 07:14:19 +0200

    :white_check_mark: add test files for untested components

---
+++ test/lib/queue.test.js
@@ -0,0 +1,9 @@
+require('module-alias/register');
+
+describe('Lib/Queue', () => {
+    const assert = require('assert');
+    const { Queue } = require('@lib/queue');
+
+    before(() => {
+    });
+});
---
+++ test/lib/worker.test.js
@@ -0,0 +1,9 @@
+require('module-alias/register');
+
+describe('Lib/Worker', () => {
+    const assert = require('assert');
+    const { Worker } = require('@lib/worker');
+
+    before(() => {
+    });
+});

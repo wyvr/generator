7339bd4ea37023910810e494260ab0b1d1cd8038
Wyvr <wyvr@daviot.at>
2021-07-22 20:44:01 +0200

    :coffin: remove packages

--- wyvr.js
+++ wyvr.js
@@ -2,20 +2,10 @@ const pkg = require('./package.json');
 
 module.exports = {
     url: 'generator.wyvr',
-    import: {
-        global: '../website/data/global.json',
-    },
     worker: {
         ratio: 0,
     },
     packages: [
-        {
-            name: 'Example',
-            path: '../theme-example',
-        },
-        {
-            path: '../broken_path_to_theme',
-        },
     ],
     assets: [
     ],

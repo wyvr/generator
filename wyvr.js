7dba707e453ce233b5d82b58a92f195709587adc
Wyvr <wyvr@daviot.at>
2021-07-22 16:34:45 +0200

    :coffin: remove package

--- wyvr.js
+++ wyvr.js
@@ -14,22 +14,14 @@ module.exports = {
             path: '../theme-example',
         },
         {
-            name: 'Startpage',
-            path: '../theme-startpage',
-        },
-        {
             path: '../broken_path_to_theme',
         },
     ],
     assets: [
-        { src: '../website/node_modules/remixicon/fonts/remixicon.css', target: 'remixicon/remixicon.css' },
-        { src: '../website/node_modules/remixicon/fonts/remixicon.ttf', target: 'remixicon/remixicon.ttf' },
-        { src: '../website/node_modules/remixicon/fonts/remixicon.woff', target: 'remixicon/remixicon.woff' },
-        { src: '../website/node_modules/remixicon/fonts/remixicon.woff2', target: 'remixicon/remixicon.woff2' },
     ],
     default_values: {
         title: 'wyvr',
-        meta_description: 'Static sites made different',
+        meta_description: 'static sites made different',
         author: pkg.author,
         version: pkg.version,
         date: new Date(),

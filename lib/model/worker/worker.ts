b27b7b04d125bc4affc050691f2800a33abf884e
wyvr <wyvr@daviot.at>
2021-10-29 12:17:34 +0200

    :rotating_light: fix lint error

--- lib/model/worker/worker.ts
+++ lib/model/worker/worker.ts
@@ -1,12 +1,13 @@
 import { WorkerStatus } from '@lib/model/worker/status';
 import cluster from 'cluster';
+import { ChildProcess } from 'child_process';
 
 export class WorkerModel {
     public status: WorkerStatus = WorkerStatus.undefined;
     public pid = 0;
-    public process: any = null;
-    constructor(custom_fork: Function = null) {
-        const instance = custom_fork ? custom_fork() : cluster.fork();
+    public process: ChildProcess = null;
+    constructor(custom_fork: () => cluster.Worker = null) {
+        const instance: cluster.Worker = custom_fork ? custom_fork() : cluster.fork();
         this.process = instance.process;
         this.pid = instance.process.pid;
     }

841a41f37f8f91b47a30df7969ecd376773e7fb8
wyvr <wyvr@daviot.at>
2021-11-02 08:39:46 +0100

    :art: refactor script worker

--- lib/worker/script.ts
+++ lib/worker/script.ts
@@ -15,31 +15,34 @@ export const script = async (value: IIdentifier[]) => {
 
     await Promise.all(
         value.map(async (identifier) => {
-            let dep_files = [];
-            ['doc', 'layout', 'page'].forEach((type) => {
-                if (identifier.file[type]) {
-                    dep_files.push(...Dependency.get_dependencies(identifier.file[type], files, identifier.dependency));
-                }
-            });
-            if (identifier.file.shortcodes) {
-                dep_files.push(...Dependency.get_dependencies(identifier.file.name, files, identifier.dependency));
-            }
-            // remove doubled dependency entries
-            dep_files = dep_files.filter((wyvr_file: WyvrFile, index) => {
-                return index == dep_files.findIndex((dep_file: WyvrFile) => dep_file.path == wyvr_file.path);
-            });
-            try {
-                // console.log(identifier.file.name, identifier.dependency, dep_files);
-                const [error, result] = await Client.create_bundle(identifier.file, dep_files);
-                if (error) {
-                    Logger.error(Error.get({ message: error }, identifier.file.name, 'worker scripts'));
-                }
-                return result;
-            } catch (e) {
-                // svelte error messages
-                Logger.error(Error.get(e, identifier.file.name, 'worker scripts'));
-            }
-            return null;
+            return await build_identifier_script(identifier, files);
         })
     );
+};
+export const build_identifier_script = async (identifier: IIdentifier, files: WyvrFile[]) => {
+    let dep_files = [];
+    ['doc', 'layout', 'page'].forEach((type) => {
+        if (identifier.file[type]) {
+            dep_files.push(...Dependency.get_dependencies(identifier.file[type], files, identifier.dependency));
+        }
+    });
+    if (identifier.file.shortcodes) {
+        dep_files.push(...Dependency.get_dependencies(identifier.file.name, files, identifier.dependency));
+    }
+    // remove doubled dependency entries
+    dep_files = dep_files.filter((wyvr_file: WyvrFile, index) => {
+        return index == dep_files.findIndex((dep_file: WyvrFile) => dep_file.path == wyvr_file.path);
+    });
+    try {
+        // console.log(identifier.file.name, identifier.dependency, dep_files);
+        const [error, result] = await Client.create_bundle(identifier.file, dep_files);
+        if (error) {
+            Logger.error(Error.get({ message: error }, identifier.file.name, 'build identifier script'));
+        }
+        return result;
+    } catch (e) {
+        // svelte error messages
+        Logger.error(Error.get(e, identifier.file.name, 'build identifier script'));
+    }
+    return null;
 };

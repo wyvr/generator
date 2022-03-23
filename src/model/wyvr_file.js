c687af04cc56cbaf998a38cb6ce1f9a116a2d88a
wyvr <wyvr@daviot.at>
2022-03-23 19:18:11 +0100

    :sparkles: add wyvr file

---
+++ src/model/wyvr_file.js
@@ -0,0 +1,38 @@
+import { extname, join, sep } from 'path';
+import { FOLDER_GEN } from '../constants/folder.js';
+import { RESERVED_KEYWORDS } from '../constants/keywords.js';
+import { WyvrFileConfig } from '../struc/wyvr_file.js';
+import { filled_string } from '../utils/validate.js';
+
+export function WyvrFile(path) {
+    let name = '', rel_path='';
+    if (filled_string(path)) {
+        const splitted = path.split(sep);
+        const gen_index = splitted.indexOf(FOLDER_GEN);
+        // make name unique per file, which is based on the path
+        // remove the gen and next (state) folder
+        const rel_splitts = gen_index > -1 ? splitted.slice(gen_index + 2) : splitted;
+        rel_path = join('@src', ...rel_splitts);
+        name = rel_splitts.join('_').replace(new RegExp(`${extname(path).replace('.', '\\.')}$`), '');
+        // avoid reserved keywords
+        if (
+            RESERVED_KEYWORDS.indexOf(name.toLowerCase()) > -1
+        ) {
+            name = `_${name}`;
+        }
+    } else {
+        path = undefined;
+    }
+
+    return {
+        name,
+        path,
+        config: WyvrFileConfig,
+        scripts: undefined,
+        styles: undefined,
+        props: undefined,
+        rel_path,
+        from_lazy: undefined
+    };
+
+}
---
+++ src/struc/wyvr_file.js
@@ -0,0 +1,18 @@
+export const WyvrHydrateDisplay = {
+    inline: 'inline',
+    block: 'block',
+};
+
+export const WyvrFileRender = { static: 'static', hydrate: 'hydrate' };
+
+export const WyvrFileLoading = { instant: 'instant', lazy: 'lazy', idle: 'idle', media: 'media', none: 'none' };
+
+export const WyvrFileConfig = {
+    display: WyvrHydrateDisplay.block,
+    render: WyvrFileRender.static,
+    loading: WyvrFileLoading.instant,
+    error: undefined,
+    portal: undefined,
+    trigger: undefined,
+    media: 'all',
+};

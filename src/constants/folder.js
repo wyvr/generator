9bcf3d0a793a18bf3b0e8e41365a7492c26e4814
wyvr <wyvr@daviot.at>
2022-04-06 11:36:35 +0200

    :wrench: add new constants

--- src/constants/folder.js
+++ src/constants/folder.js
@@ -3,3 +3,6 @@ import { join } from 'path';
 export const FOLDER_GEN = 'gen';
 export const FOLDER_SRC = 'src';
 export const FOLDER_GEN_SRC = join(FOLDER_GEN, FOLDER_SRC);
+export const FOLDER_ASSETS = 'assets';
+export const FOLDER_GEN_ASSETS = join(FOLDER_GEN, FOLDER_ASSETS);
+export const FOLDER_LIST_PACKAGE_COPY = ['assets', 'routes', 'plugins', 'exec'];

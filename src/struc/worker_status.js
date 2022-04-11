68c91096a92546357133e4355838001efb593a8c
wyvr <wyvr@daviot.at>
2022-04-11 15:35:28 +0200

    :sparkles: add worker status

---
+++ src/struc/worker_status.js
@@ -0,0 +1,11 @@
+export const WorkerStatus = {
+    undefined: 0,
+    exists: 1,
+    done: 2,
+    idle: 3,
+    busy: 4,
+    dead: 5,
+};
+export function get_name(status) {
+    return Object.keys(WorkerStatus).find((key) => WorkerStatus[key] == status);
+}
---
+++ test/struc/worker_status/get_name.js
@@ -0,0 +1,15 @@
+import { strictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { WorkerStatus, get_name } from '../../../src/struc/worker_status.js';
+
+describe('struc/worker_status/get_name', () => {
+    it('undefined', () => {
+        strictEqual(get_name(undefined), undefined);
+    });
+    it('unknown value value', () => {
+        strictEqual(get_name('nope'), undefined);
+    });
+    it('dead', () => {
+        strictEqual(get_name(WorkerStatus.dead), 'dead');
+    });
+});

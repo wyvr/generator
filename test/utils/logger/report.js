c6033c15408616aa9c5b9503dff0e71d2fec1346
wyvr <wyvr@daviot.at>
2022-04-18 21:04:40 +0200

    :white_check_mark: improve logger report coverage

--- test/utils/logger/report.js
+++ test/utils/logger/report.js
@@ -4,6 +4,8 @@ import { describe, it } from 'mocha';
 import { LogColor, LogIcon } from '../../../src/struc/log.js';
 import { Logger } from '../../../src/utils/logger.js';
 import { Report } from '../../../src/vars/report.js';
+import Sinon from 'sinon';
+
 
 describe('utils/logger/report', () => {
     let log, err;
@@ -11,6 +13,18 @@ describe('utils/logger/report', () => {
 
     const icon = LogColor.report(LogIcon.report);
     const color = LogColor.report;
+
+
+    before(() => {
+        Sinon.stub(Logger, 'output');
+        Logger.output.callsFake((...msg) => {
+            logger_messages.push(msg.slice(3));
+        });
+    });
+    after(() => {
+        Logger.output.restore();
+    });
+
     before(() => {
         // runs once before the first test in this block
         log = console.log;
@@ -45,6 +59,11 @@ describe('utils/logger/report', () => {
     });
 
     it('key + multiple text', () => {
+        Logger.report(500, 'a', 'b');
+        deepStrictEqual(result, [[icon, color(`a b 500 ${kleur.dim('ms')}`)]]);
+    });
+    it('from worker', () => {
+        
         Logger.report(500, 'a', 'b');
         deepStrictEqual(result, [[icon, color(`a b 500 ${kleur.dim('ms')}`)]]);
     });

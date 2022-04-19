9ecdc2df155c55192d4de1601fa2f19203ad362e
wyvr <wyvr@daviot.at>
2022-04-19 17:37:13 +0200

    :sparkles: add to_snake_case

--- src/utils/to.js
+++ src/utils/to.js
@@ -1,4 +1,4 @@
-import { is_object, is_array, is_null } from './validate.js';
+import { is_object, is_array, is_null, filled_string } from './validate.js';
 
 export function to_string(value) {
     if (is_null(value)) {
@@ -8,4 +8,16 @@ export function to_string(value) {
         return JSON.stringify(value);
     }
     return value.toString();
+}
+
+export function to_snake_case(value) {
+    if(!filled_string(value)) {
+        return undefined;
+    }
+    return value
+        .replace(/([A-Z])/g, '_$1')
+        .toLowerCase()
+        .replace(/^_/, '')
+        .replace(/[^a-z_]/g, '_')
+        .replace(/_+/g, '_');
 }
---
+++ test/utils/to/to_snake_case.test.js
@@ -0,0 +1,69 @@
+import { strictEqual, deepStrictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { to_snake_case } from '../../../src/utils/to.js';
+
+describe('utils/to/to_snake_case', () => {
+    it('undefined', () => {
+        strictEqual(to_snake_case(), undefined);
+    });
+    it('null', () => {
+        strictEqual(to_snake_case(null), undefined);
+    });
+    it('empty string', () => {
+        strictEqual(to_snake_case(''), undefined);
+    });
+    it('string', () => {
+        strictEqual(to_snake_case('huhu'), 'huhu');
+    });
+    it('string Pascal case', () => {
+        strictEqual(to_snake_case('HuHu'), 'hu_hu');
+    });
+    it('string Camel case', () => {
+        strictEqual(to_snake_case('huHu'), 'hu_hu');
+    });
+    it('string Snake case', () => {
+        strictEqual(to_snake_case('hu_hu'), 'hu_hu');
+    });
+    it('string Kebab case', () => {
+        strictEqual(to_snake_case('hu-hu'), 'hu_hu');
+    });
+    it('string mixed cases', () => {
+        strictEqual(to_snake_case('Hu-_-Hu'), 'hu_hu');
+    });
+    it('int', () => {
+        strictEqual(to_snake_case(1), undefined);
+    });
+    it('float', () => {
+        strictEqual(to_snake_case(1.1), undefined);
+    });
+    it('bigint', () => {
+        strictEqual(to_snake_case(BigInt(Number.MAX_SAFE_INTEGER)), undefined);
+    });
+    it('empty array', () => {
+        strictEqual(to_snake_case([]), undefined);
+    });
+    it('array', () => {
+        strictEqual(to_snake_case(['a']), undefined);
+    });
+    it('empty object', () => {
+        strictEqual(to_snake_case({}), undefined);
+    });
+    it('object', () => {
+        strictEqual(to_snake_case({ a: true }), undefined);
+    });
+    it('boolean true', () => {
+        strictEqual(to_snake_case(true), undefined);
+    });
+    it('boolean false', () => {
+        strictEqual(to_snake_case(false), undefined);
+    });
+    it('symbol', () => {
+        strictEqual(to_snake_case(Symbol('foo')), undefined);
+    });
+    it('date', () => {
+        strictEqual(to_snake_case(new Date()), undefined);
+    });
+    it('regex', () => {
+        strictEqual(to_snake_case(/.*/), undefined);
+    });
+});

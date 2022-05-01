25d631df8372d48dfbee6bb69f819d1fd85d2fcf
wyvr <wyvr@daviot.at>
2022-05-01 19:47:15 +0200

    :sparkles: test present

--- src/action/present.js
+++ src/action/present.js
@@ -1,24 +1,32 @@
 import { Logger } from '../utils/logger.js';
-import { is_object } from '../utils/validate.js';
+import { filled_array, is_object } from '../utils/validate.js';
 import { Cwd } from '../vars/cwd.js';
 import { Env } from '../vars/env.js';
 import { UniqId } from '../vars/uniq_id.js';
 
-export function present(config, command) {
-    // UniqId.set(uniq);
-    
+export function present(config_data) {
     const id = UniqId.get();
+    let formatted_id = id.substring(0, 5);
+    if (id.length > 5) {
+        formatted_id += Logger.color.dim(id.substring(5));
+    }
+    let command = config_data?.cli?.command;
+    if (!filled_array(command)) {
+        command = ['-'];
+    }
+    command = command.join(' ')
     // prepare process
     process.title = `wyvr ${command} ${process.pid}`;
     // present some variables
     Logger.present('pid', process.pid, Logger.color.dim(`${process.title}`));
     Logger.present('cwd', Cwd.get());
-    Logger.present('id', `${id.substring(0, 5)}${Logger.color.dim(id.substring(5))}`);
+
+    Logger.present('id', formatted_id);
     Logger.present('environment', Env.name());
-    
+
     let flags = '';
-    if (is_object(config.cli.flags)) {
-        flags = Object.keys(config.cli.flags).join(' ');
+    if (is_object(config_data?.cli?.flags)) {
+        flags = Object.keys(config_data.cli.flags).join(' ');
     }
     Logger.present('command', command, Logger.color.dim(flags));
 }
---
+++ test/action/present.test.js
@@ -0,0 +1,81 @@
+import { strictEqual, deepStrictEqual } from 'assert';
+import { existsSync, mkdirSync, rmdirSync } from 'fs';
+import { describe, it } from 'mocha';
+import { dirname, join, resolve } from 'path';
+import Sinon from 'sinon';
+import { fileURLToPath } from 'url';
+import { present } from '../../src/action/present.js';
+import { Logger } from '../../src/utils/logger.js';
+import { Cwd } from '../../src/vars/cwd.js';
+import { UniqId } from '../../src/vars/uniq_id.js';
+
+describe('action/present', () => {
+    const __dirname = dirname(resolve(join(fileURLToPath(import.meta.url))));
+    const __root = join(__dirname, '..', '..');
+    const __path = join('test', 'action', '_tests', 'present');
+    const test_folder = join(__root, __path);
+    let sandbox;
+    let exit_code;
+    let logger_messages = [];
+
+    before(() => {
+        Cwd.set(__root);
+        sandbox = Sinon.createSandbox();
+        sandbox.stub(process, 'exit');
+        process.exit.callsFake((code) => {
+            exit_code = code;
+        });
+        sandbox.stub(process, 'pid').value(12345);
+        sandbox.stub(Logger, 'output');
+        Logger.output.callsFake((...msg) => {
+            logger_messages.push(msg.slice(3));
+        });
+    });
+    beforeEach(() => {
+        UniqId.value = '01234567890';
+        mkdirSync(test_folder, { recursive: true });
+    });
+    afterEach(() => {
+        exit_code = undefined;
+        UniqId.value = undefined;
+        logger_messages = [];
+        if (existsSync(test_folder)) {
+            rmdirSync(test_folder, { recursive: true, force: true });
+        }
+    });
+    after(() => {
+        Cwd.set(undefined);
+        sandbox.restore();
+    });
+    it('undefined', async () => {
+        present();
+        deepStrictEqual(logger_messages, [
+            ['pid', '\x1B[32m12345\x1B[39m', '\x1B[2mwyvr - 12345\x1B[22m'],
+            ['cwd', '\x1B[32m' + __root + '\x1B[39m'],
+            ['id', '\x1B[32m01234\x1B[2m567890\x1B[22m\x1B[39m'],
+            ['environment', '\x1B[32mprod\x1B[39m'],
+            ['command', '\x1B[32m-\x1B[39m', '\x1B[2m\x1B[22m'],
+        ]);
+    });
+    it('short id', async () => {
+        UniqId.value = '0';
+        present();
+        deepStrictEqual(logger_messages, [
+            ['pid', '\x1B[32m12345\x1B[39m', '\x1B[2mwyvr - 12345\x1B[22m'],
+            ['cwd', '\x1B[32m' + __root + '\x1B[39m'],
+            ['id', '\x1B[32m0\x1B[39m'],
+            ['environment', '\x1B[32mprod\x1B[39m'],
+            ['command', '\x1B[32m-\x1B[39m', '\x1B[2m\x1B[22m'],
+        ]);
+    });
+    it('commands and flags', async () => {
+        present({ cli: { command: ['build', 'the', 'site'], flags: { test: true, flag: true } } });
+        deepStrictEqual(logger_messages, [
+            ['pid', '\x1B[32m12345\x1B[39m', '\x1B[2mwyvr build the site 12345\x1B[22m'],
+            ['cwd', '\x1B[32m' + __root + '\x1B[39m'],
+            ['id', '\x1B[32m01234\x1B[2m567890\x1B[22m\x1B[39m'],
+            ['environment', '\x1B[32mprod\x1B[39m'],
+            ['command', '\x1B[32mbuild the site\x1B[39m', '\x1B[2mtest flag\x1B[22m'],
+        ]);
+    });
+});

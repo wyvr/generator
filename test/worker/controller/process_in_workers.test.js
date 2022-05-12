8a5da22c0d4a041aca3a15cde35e01a680b9c316
wyvr <wyvr@daviot.at>
2022-05-12 22:43:25 +0200

    :white_check_mark: test process in worker

--- test/worker/controller/process_in_workers.test.js
+++ test/worker/controller/process_in_workers.test.js
@@ -4,6 +4,7 @@ import Sinon from 'sinon';
 import { Queue } from '../../../src/model/queue.js';
 import { WorkerAction } from '../../../src/struc/worker_action.js';
 import { WorkerStatus } from '../../../src/struc/worker_status.js';
+import { Event } from '../../../src/utils/event.js';
 import { WorkerController } from '../../../src/worker/controller.js';
 
 describe('worker/controller/process_in_workers', () => {
@@ -69,5 +70,202 @@ describe('worker/controller/process_in_workers', () => {
         strictEqual(result, false);
         strictEqual(exit_code, 0);
         deepStrictEqual(logger_messages, [['\x1B[31m✖\x1B[39m', '\x1B[31munknown action\x1B[39m']]);
+    });
+    it('correct batch size', async () => {
+        WorkerController.workers = new Array(2).fill(true).map((item, index) => {
+            const pid = 1000 + index;
+            return {
+                pid,
+                status: WorkerStatus.idle,
+                process: {
+                    send: (data) => {
+                        send_data.push(data);
+                        setTimeout(() => {
+                            WorkerController.workers.find((ref_worker) => {
+                                if (ref_worker.pid == pid) {
+                                    ref_worker.status = WorkerStatus.done;
+                                    return true;
+                                }
+                            });
+                            Event.emit('worker_status', WorkerStatus.done, { pid });
+                            setTimeout(() => {
+                                WorkerController.workers.find((ref_worker) => {
+                                    if (ref_worker.pid == pid) {
+                                        ref_worker.status = WorkerStatus.idle;
+                                        return true;
+                                    }
+                                });
+                                Event.emit('worker_status', WorkerStatus.idle, { pid });
+                            }, 0);
+                        }, 0);
+                    },
+                },
+            };
+        });
+        WorkerController.worker_amount = WorkerController.workers.length;
+        const list = new Array(10).fill(true);
+        const result = await WorkerController.process_in_workers(WorkerAction.log, list, 1000);
+        strictEqual(result, true);
+        strictEqual(exit_code, 0);
+        deepStrictEqual(send_data, [
+            {
+                action: {
+                    key: 0,
+                    value: [true, true, true, true, true],
+                },
+            },
+            {
+                action: {
+                    key: 0,
+                    value: [true, true, true, true, true],
+                },
+            },
+        ]);
+        deepStrictEqual(logger_messages, [
+            ['\x1B[34mℹ\x1B[39m', 'process \x1B[34m10\x1B[39m items, batch size \x1B[36m1000\x1B[39m'],
+        ]);
+    });
+    it('single item list', async () => {
+        WorkerController.workers = new Array(2).fill(true).map((item, index) => {
+            const pid = 1000 + index;
+            return {
+                pid,
+                status: WorkerStatus.idle,
+                process: {
+                    send: (data) => {
+                        send_data.push(data);
+                        setTimeout(() => {
+                            WorkerController.workers.find((ref_worker) => {
+                                if (ref_worker.pid == pid) {
+                                    ref_worker.status = WorkerStatus.done;
+                                    return true;
+                                }
+                            });
+                            Event.emit('worker_status', WorkerStatus.done, { pid });
+                            setTimeout(() => {
+                                WorkerController.workers.find((ref_worker) => {
+                                    if (ref_worker.pid == pid) {
+                                        ref_worker.status = WorkerStatus.idle;
+                                        return true;
+                                    }
+                                });
+                                Event.emit('worker_status', WorkerStatus.idle, { pid });
+                            }, 0);
+                        }, 0);
+                    },
+                },
+            };
+        });
+        WorkerController.worker_amount = WorkerController.workers.length;
+        const list = [true];
+        const result = await WorkerController.process_in_workers(WorkerAction.log, list, 1000);
+        strictEqual(result, true);
+        strictEqual(exit_code, 0);
+        deepStrictEqual(send_data, [
+            {
+                action: {
+                    key: 0,
+                    value: [true],
+                },
+            }
+        ]);
+        deepStrictEqual(logger_messages, [
+            ['\x1B[34mℹ\x1B[39m', 'process \x1B[34m1\x1B[39m item, batch size \x1B[36m1000\x1B[39m'],
+        ]);
+    });
+    it('empty list', async () => {
+        WorkerController.workers = new Array(2).fill(true).map((item, index) => {
+            const pid = 1000 + index;
+            return {
+                pid,
+                status: WorkerStatus.idle,
+                process: {
+                    send: (data) => {
+                        send_data.push(data);
+                        setTimeout(() => {
+                            WorkerController.workers.find((ref_worker) => {
+                                if (ref_worker.pid == pid) {
+                                    ref_worker.status = WorkerStatus.done;
+                                    return true;
+                                }
+                            });
+                            Event.emit('worker_status', WorkerStatus.done, { pid });
+                            setTimeout(() => {
+                                WorkerController.workers.find((ref_worker) => {
+                                    if (ref_worker.pid == pid) {
+                                        ref_worker.status = WorkerStatus.idle;
+                                        return true;
+                                    }
+                                });
+                                Event.emit('worker_status', WorkerStatus.idle, { pid });
+                            }, 0);
+                        }, 0);
+                    },
+                },
+            };
+        });
+        WorkerController.worker_amount = WorkerController.workers.length;
+        const list = [];
+        const result = await WorkerController.process_in_workers(WorkerAction.log, list, 1000);
+        strictEqual(result, true);
+        strictEqual(exit_code, 0);
+        deepStrictEqual(send_data, []);
+        deepStrictEqual(logger_messages, [
+            ['\x1B[35m♥\x1B[39m', '\x1B[35mno items to process, batch size \x1B[36m1000\x1B[39m\x1B[35m\x1B[39m'],
+        ]);
+    });
+    it('empty batch size', async () => {
+        WorkerController.workers = new Array(2).fill(true).map((item, index) => {
+            const pid = 1000 + index;
+            return {
+                pid,
+                status: WorkerStatus.idle,
+                process: {
+                    send: (data) => {
+                        send_data.push(data);
+                        setTimeout(() => {
+                            WorkerController.workers.find((ref_worker) => {
+                                if (ref_worker.pid == pid) {
+                                    ref_worker.status = WorkerStatus.done;
+                                    return true;
+                                }
+                            });
+                            Event.emit('worker_status', WorkerStatus.done, { pid });
+                            setTimeout(() => {
+                                WorkerController.workers.find((ref_worker) => {
+                                    if (ref_worker.pid == pid) {
+                                        ref_worker.status = WorkerStatus.idle;
+                                        return true;
+                                    }
+                                });
+                                Event.emit('worker_status', WorkerStatus.idle, { pid });
+                            }, 0);
+                        }, 0);
+                    },
+                },
+            };
+        });
+        WorkerController.worker_amount = WorkerController.workers.length;
+        const list = new Array(10).fill(true);
+        const result = await WorkerController.process_in_workers(WorkerAction.log, list);
+        strictEqual(result, true);
+        strictEqual(exit_code, 0);
+        deepStrictEqual(send_data, [
+            {
+                action: {
+                    key: 0,
+                    value: [true, true, true, true, true],
+                },
+            },
+            {
+                action: {
+                    key: 0,
+                    value: [true, true, true, true, true],
+                },
+            },
+        ]);
+        deepStrictEqual(logger_messages, [
+            ['\x1B[34mℹ\x1B[39m', 'process \x1B[34m10\x1B[39m items, batch size \x1B[36m10\x1B[39m'],
+        ]);
     });
 });

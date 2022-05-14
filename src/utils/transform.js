d7ad7dbd696a1c70e2b80571fda48b7d0e01e0ee
wyvr <wyvr@daviot.at>
2022-05-14 22:06:31 +0200

    :sparkles: add extract tags from content

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,7 +1,7 @@
 import { dirname, join, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { exists, read, to_extension } from './file.js';
-import { filled_string, is_null } from './validate.js';
+import { filled_string, is_null, is_number } from './validate.js';
 
 const __dirname = dirname(resolve(join(fileURLToPath(import.meta.url), '..')));
 
@@ -33,7 +33,7 @@ export function combine_splits(path, content) {
         content = `${content}<style>${css_content}</style>`;
         result.css = css;
     }
-    
+
     // load js
     const js = to_extension(path, 'js');
     if (exists(js)) {
@@ -43,5 +43,41 @@ export function combine_splits(path, content) {
     }
 
     result.content = content;
+    return result;
+}
+
+export function extract_tags_from_content(content, tag, max) {
+    const result = {
+        content,
+        tags: [],
+    };
+    if (!filled_string(content) || !filled_string(tag)) {
+        return result;
+    }
+    tag = tag.toLowerCase().trim();
+    const use_max = is_number(max) && max > 0;
+
+    let search_tag = true;
+    const tag_start = `<${tag}`;
+    const tag_end = `</${tag}>`;
+    let tag_start_index, tag_end_index;
+    while (search_tag) {
+        tag_start_index = content.indexOf(tag_start);
+        tag_end_index = content.indexOf(tag_end);
+        if (tag_start_index > -1 && tag_end_index > -1) {
+            // append the tag into the result
+            result.tags.push(content.slice(tag_start_index, tag_end_index + tag_end.length));
+            // remove the script from the content
+            content = content.substr(0, tag_start_index) + content.substr(tag_end_index + tag_end.length);
+            // allow that not all tags should be extracted
+            if (use_max && result.tags.length == max) {
+                search_tag = false;
+            }
+            continue;
+        }
+        search_tag = false;
+    }
+    result.content = content;
+
     return result;
 }
---
+++ test/utils/transform/extract_tags_from_content.test.js
@@ -0,0 +1,69 @@
+import { deepStrictEqual, strictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { dirname, join, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { extract_tags_from_content } from '../../../src/utils/transform.js';
+
+describe('utils/transform/extract_tags_from_content', () => {
+    const __dirname = join(
+        dirname(resolve(join(fileURLToPath(import.meta.url)))),
+        '_tests',
+        'extract_tags_from_content'
+    );
+
+    it('undefined', () => {
+        deepStrictEqual(extract_tags_from_content(), {
+            content: undefined,
+            tags: [],
+        });
+    });
+    it('empty content', () => {
+        const content = '';
+        deepStrictEqual(extract_tags_from_content(content), {
+            content,
+            tags: [],
+        });
+    });
+    it('empty content, extract tag', () => {
+        const content = '';
+        deepStrictEqual(extract_tags_from_content(content, 'script'), {
+            content,
+            tags: [],
+        });
+    });
+    it('content, extract tag', () => {
+        const content = `<html><script>var a = 0;</script></html>`;
+        deepStrictEqual(extract_tags_from_content(content, 'script'), {
+            content: '<html></html>',
+            tags: ['<script>var a = 0;</script>'],
+        });
+    });
+    it('content, extract mulitple tags', () => {
+        const content = `<html><script>var a = 0;</script> test <script>var b = 0;</script></html>`;
+        deepStrictEqual(extract_tags_from_content(content, 'script'), {
+            content: '<html> test </html>',
+            tags: ['<script>var a = 0;</script>', '<script>var b = 0;</script>'],
+        });
+    });
+    it('invalid code', () => {
+        const content = `<html><script>var a = 0;</html>`;
+        deepStrictEqual(extract_tags_from_content(content, 'script'), {
+            content,
+            tags: [],
+        });
+    });
+    it('self closing tag', () => {
+        const content = `<html><script /></html>`;
+        deepStrictEqual(extract_tags_from_content(content, 'script'), {
+            content,
+            tags: [],
+        });
+    });
+    it('content, extract only first tag', () => {
+        const content = `<html><script>var a = 0;</script> test <script>var b = 0;</script></html>`;
+        deepStrictEqual(extract_tags_from_content(content, 'script', 1), {
+            content: '<html> test <script>var b = 0;</script></html>',
+            tags: ['<script>var a = 0;</script>'],
+        });
+    });
+});

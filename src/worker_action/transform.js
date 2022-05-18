56d7365d6aeaf59528fab6bc66ce71f16fce2710
wyvr <wyvr@daviot.at>
2022-05-18 09:12:24 +0200

    :construction: begin with transform in worker

--- src/worker_action/transform.js
+++ src/worker_action/transform.js
@@ -1,5 +1,9 @@
 // import { Config } from '../utils/config.js';
+import { extname } from 'path';
+import { read, remove, write } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
+import { combine_splits } from '../utils/transform.js';
+import { filled_string } from '../utils/validate.js';
 // import { match_interface } from '../utils/validate.js';
 // import { Cwd } from '../vars/cwd.js';
 // import { Env } from '../vars/env.js';
@@ -9,7 +13,20 @@ import { Logger } from '../utils/logger.js';
 // import { WyvrPath } from '../vars/wyvr_path.js';
 
 export async function transform(files) {
-    Logger.info('transform', files);
-    await new Promise(r => setTimeout(r, 2000));
+    for (const file of files) {
+        const extension = extname(file);
+        let content = read(file);
+        if (extension == '.svelte') {
+            const combined = await combine_splits(file, content);
+            if (filled_string(combined.content)) {
+                content = combined.content;
+            }
+            remove(combined.css)
+            remove(combined.js)
+            // override the content
+            write(file, content);
+        }
+    }
+    // await new Promise(r => setTimeout(r, 2000));
     return false;
 }

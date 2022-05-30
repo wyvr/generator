a44c0fd2eba4f4a6364435c2d591091f4b4bb0e8
wyvr <wyvr@daviot.at>
2022-05-30 20:26:27 +0200

    :sparkles: add uniq_values func

--- src/utils/uniq.js
+++ src/utils/uniq.js
@@ -1,5 +1,16 @@
 import { v4 } from 'uuid';
+import { is_null, is_array } from './validate.js';
 
 export function uniq() {
     return v4().replace(/-/g, '');
+}
+export function uniq_values(list) {
+    if(is_null(list)) {
+        return [];
+    }
+    // force array
+    if(!is_array(list)) {
+        list = [list];
+    }
+    return list.filter((x, index, arr) => arr.indexOf(x) == index);
 }
---
+++ test/utils/uniq/uniq_values.test.js
@@ -0,0 +1,33 @@
+import { deepStrictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { uniq_values } from '../../../src/utils/uniq.js';
+
+describe('utils/uniq/uniq_values', () => {
+    it('undefined', () => {
+        deepStrictEqual(uniq_values(), []);
+    });
+    it('null', () => {
+        deepStrictEqual(uniq_values(null), []);
+    });
+    it('empty string', () => {
+        deepStrictEqual(uniq_values(['']), ['']);
+    });
+    it('string', () => {
+        deepStrictEqual(uniq_values('huhu'), ['huhu']);
+    });
+    it('number', () => {
+        deepStrictEqual(uniq_values(1), [1]);
+    });
+    it('empty array', () => {
+        deepStrictEqual(uniq_values([]), []);
+    });
+    it('array', () => {
+        deepStrictEqual(uniq_values(['a', 'b', 'a']), ['a', 'b']);
+    });
+    it('number string mixed', () => {
+        deepStrictEqual(uniq_values(['1', 1]), ['1', 1]);
+    });
+    it('object', () => {
+        deepStrictEqual(uniq_values([{ a: true }, { b: true }, { a: true }]), [{ a: true }, { b: true }, { a: true }]);
+    });
+});

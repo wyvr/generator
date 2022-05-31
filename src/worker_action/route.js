2316094d8dffc808bfbd7da10b0eef9694646ec3
wyvr <wyvr@daviot.at>
2022-05-31 07:09:31 +0200

    :bug: add failover for route execution

--- src/worker_action/route.js
+++ src/worker_action/route.js
@@ -2,16 +2,19 @@ import { WyvrData } from '../model/wyvr_data.js';
 import { Config } from '../utils/config.js';
 import { execute_route, write_routes } from '../utils/route.js';
 import { set_default_values } from '../utils/transform.js';
-import { filled_array } from '../utils/validate.js';
+import { filled_array, is_null } from '../utils/validate.js';
 
 export async function route(files) {
     if (!filled_array(files)) {
         return;
     }
     const default_values = Config.get('default_values');
-    
+
     for (const route of files) {
         const wyvr_pages = await execute_route(route);
+        if (is_null(wyvr_pages)) {
+            continue;
+        }
         write_routes(wyvr_pages, (wyvr_page) => {
             // enhance the data from the pages
             // set default values when the key is not available in the given data

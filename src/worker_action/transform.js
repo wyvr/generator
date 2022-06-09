40daa0966a2bc46b8efc1df6cd8b151d94dc3719
wyvr <wyvr@daviot.at>
2022-06-09 21:26:16 +0200

    :fire: remove compiling from transform

--- src/worker_action/transform.js
+++ src/worker_action/transform.js
@@ -1,18 +1,9 @@
-// import { Config } from '../utils/config.js';
 import { extname } from 'path';
 import { insert_import } from '../utils/compile.js';
 import { exists, read, symlink, write } from '../utils/file.js';
 import { to_client_path, to_server_path } from '../utils/to.js';
-// import { Logger } from '../utils/logger.js';
-import { combine_splits, replace_wyvr_magic } from '../utils/transform.js';
+import { combine_splits } from '../utils/transform.js';
 import { filled_array, filled_string } from '../utils/validate.js';
-// import { match_interface } from '../utils/validate.js';
-// import { Cwd } from '../vars/cwd.js';
-// import { Env } from '../vars/env.js';
-// import { ReleasePath } from '../vars/release_path.js';
-// import { Report } from '../vars/report.js';
-// import { UniqId } from '../vars/uniq_id.js';
-// import { WyvrPath } from '../vars/wyvr_path.js';
 
 export async function transform(files) {
     if (!filled_array(files)) {
@@ -23,8 +14,6 @@ export async function transform(files) {
             continue;
         }
         const extension = extname(file);
-        const server_file = to_server_path(file);
-        const client_file = to_client_path(file);
         if (extension == '.svelte') {
             let content = read(file);
             const combined = await combine_splits(file, content);
@@ -35,15 +24,6 @@ export async function transform(files) {
             // override the content
             write(file, content);
 
-            // generate server file
-            const server_code = replace_wyvr_magic(content, false);
-            if (server_code) {
-                write(server_file, server_code);
-            }
-
-            // generate client file
-            write(client_file, replace_wyvr_magic(content, true));
-
             continue;
         }
         // replace import in text files
@@ -52,8 +32,8 @@ export async function transform(files) {
             write(file, expanded_content);
         }
         // link static files
-        symlink(file, server_file);
-        symlink(file, client_file);
+        symlink(file, to_server_path(file));
+        symlink(file, to_client_path(file));
     }
     return true;
 }

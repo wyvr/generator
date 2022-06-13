feeff71925f0237e1737a6546a3abe7e72686cc6
wyvr <wyvr@daviot.at>
2022-06-13 22:36:23 +0200

    :sparkles: enable deactived features

--- src/action/build.js
+++ src/action/build.js
@@ -2,8 +2,10 @@ import { FOLDER_GEN_DATA } from '../constants/folder.js';
 import { WorkerAction } from '../struc/worker_action.js';
 import { get_name, WorkerEmit } from '../struc/worker_emit.js';
 import { nano_to_milli } from '../utils/convert.js';
+import { Event } from '../utils/event.js';
 import { collect_files } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
+import { Plugin } from '../utils/plugin.js';
 import { WorkerController } from '../worker/controller.js';
 import { wait_until_idle } from './wait_until_idle.js';
 
@@ -18,22 +20,27 @@ export async function build() {
 
     const list = collect_files(FOLDER_GEN_DATA, 'json');
     const identifiers = {};
-    // const listener_id = Event.on('emit', identifier_name, (data) => {
-    //     if (!data) {
-    //         return;
-    //     }
-    //     delete data.type;
-    //     identifiers[data.identifier] = data;
-    // });
+    const listener_id = Event.on('emit', identifier_name, (data) => {
+        if (!data) {
+            return;
+        }
+        delete data.type;
+        identifiers[data.identifier] = data;
+    });
 
-    // await Plugin.before(name, list);
+    await Plugin.before(name, list);
 
     await WorkerController.process_in_workers(WorkerAction.build, list, 100);
 
-    // Event.off('emit', identifier_name, listener_id);
-    // Logger.info('found', Object.keys(identifiers).length, 'identifiers', Logger.color.dim('different layout combinations'));
+    Event.off('emit', identifier_name, listener_id);
+    Logger.info(
+        'found',
+        Object.keys(identifiers).length,
+        'identifiers',
+        Logger.color.dim('different layout combinations')
+    );
 
-    // await Plugin.after(name, list);
+    await Plugin.after(name, list);
 
-    Logger.stop(name, nano_to_milli(process.hrtime.bigint() - start));    
+    Logger.stop(name, nano_to_milli(process.hrtime.bigint() - start));
 }

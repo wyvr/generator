5e56f63b4493ed43aeb63f80e8c3b7afb261d874
wyvr <wyvr@daviot.at>
2022-06-14 08:55:59 +0200

    :construction: begin adding i18n and inject config

--- src/utils/compile_svelte.js
+++ src/utils/compile_svelte.js
@@ -11,6 +11,9 @@ import { Cwd } from '../vars/cwd.js';
 import { FOLDER_GEN_SERVER, FOLDER_GEN_SRC, FOLDER_GEN_TEMP } from '../constants/folder.js';
 import { search_segment } from './segment.js';
 import { replace_src_in_path, replace_src_path } from './transform.js';
+import { register_inject_config } from './global.js';
+import { inject_config } from './config.js';
+import { get_language, register_i18n } from './i18n.js';
 
 export async function compile_server_svelte_from_code(content, file) {
     if (!filled_string(content) || !filled_string(file)) {
@@ -53,7 +56,9 @@ export async function compile_server_svelte_from_code(content, file) {
             return `import ${imported} from '${path}'`;
         });
 
-        const resourced_content = replace_src_path(modified_content, FOLDER_GEN_SERVER, extname(file));
+        const resourced_content = await inject_config(
+            replace_src_path(modified_content, FOLDER_GEN_SERVER, extname(file))
+        );
 
         // compile svelte
         const compiled = await compile(resourced_content, {
@@ -97,9 +102,106 @@ export async function execute_server_compiled_svelte(compiled, file) {
     return component;
 }
 
-export async function render_server_compiled_svelte(exec_result, file) {
-    if (!match_interface(exec_result, { compiled: true, component: true, result: true }) || !filled_string(file)) {
+export async function render_server_compiled_svelte(exec_result, data, file) {
+    if (
+        !match_interface(exec_result, { compiled: true, component: true, result: true }) ||
+        !filled_string(file) ||
+        is_null(data)
+    ) {
         return undefined;
     }
-    return undefined;
+    // set properties
+    // const propNames = Object.keys(data);
+    // if (Array.isArray(propNames) && Array.isArray(exec_result.compiled.vars)) {
+    //     // check for not used props
+    //     const unused_props = propNames.filter((prop) => {
+    //         return (
+    //             exec_result.compiled.vars.find((v) => {
+    //                 return v.name == prop;
+    //             }) == null
+    //         );
+    //     });
+    //     if (unused_props.length > 0) {
+    //         exec_result.notes.push({ msg: 'unused props', details: unused_props });
+    //     }
+    // }
+
+
+    register_inject_config();
+    // set the correct translations for the page
+    register_i18n();
+    global.wyvr_i18n.set(get_language(data?._wyvr.language));
+    console.log('#', get_language(data?._wyvr.language))
+    try {
+        exec_result.result = await exec_result.component.render(data);
+    } catch (e) {
+        Logger.error(get_error_message(e, file, 'svelte server render'));
+        return undefined;
+    }
+
+    return exec_result;
 }
+// static async render(svelte_render_item, props) {
+//     const propNames = Object.keys(props);
+//     if (Array.isArray(propNames) && Array.isArray(svelte_render_item.compiled.vars)) {
+//         // check for not used props
+//         const unused_props = propNames.filter((prop) => {
+//             return (
+//                 svelte_render_item.compiled.vars.find((v) => {
+//                     return v.name == prop;
+//                 }) == null
+//             );
+//         });
+//         if (unused_props.length > 0) {
+//             svelte_render_item.notes.push({ msg: 'unused props', details: unused_props });
+//         }
+//     }
+
+//     // set the correct translations for the page
+//     I18N.setup();
+//     const translations = I18N.get(props._wyvr.language);
+//     I18N.i18n.init(translations);
+//     try {
+//         svelte_render_item.result = await svelte_render_item.component.render(props);
+//     } catch (e) {
+//         return [e, null];
+//     }
+//     // write css file
+//     const css_file_path = join('gen', 'css', `${props._wyvr.identifier.replace(/\./g, '-')}.css`);
+//     const identifier_item = {
+//         url: props.url,
+//         identifier: props._wyvr.identifier,
+//         extension: props._wyvr.extension,
+//     };
+//     let media_files = {};
+//     if (!fs.existsSync(css_file_path)) {
+//         media_files = await this.write_css_file(css_file_path, svelte_render_item.result.css.code);
+//     } else {
+//         const last_modified = fs.statSync(css_file_path).mtime;
+//         // changes in the time range of 5 seconds avoids recreation of css files
+//         // @WARN when hugh amounts of data gets generated css files can be written multiple times
+//         if (new Date().getTime() - new Date(last_modified).getTime() > 5000) {
+//             media_files = await this.write_css_file(css_file_path, svelte_render_item.result.css.code);
+//         }
+//     }
+//     // when there are media files returned, create them
+//     await this.write_media_files(css_file_path, media_files);
+//     if (Object.keys(media_files).length > 0) {
+//         // inject media css files
+//         svelte_render_item.result.html = await this.inject_media_files(
+//             svelte_render_item.result.html,
+//             css_file_path,
+//             media_files
+//         );
+//     }
+//     // inject translations
+//     if (translations) {
+//         svelte_render_item.result.html = svelte_render_item.result.html.replace(
+//             /<\/body>/,
+//             `<script>var wyvr_i18n_tr = ${JSON.stringify(translations)}</script></body>`
+//         );
+//     }
+
+//     // svelte_render_item.result.html = svelte_render_item.result.html.replace('</head>', `<style>${svelte_render_item.result.css.code}</style></head>`);
+//     return [null, svelte_render_item, identifier_item];
+// }
--- src/worker_action/build.js
+++ src/worker_action/build.js
@@ -3,6 +3,7 @@ import { read_json } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
 import { generate_page_code } from '../utils/generate.js';
 import { filled_array, } from '../utils/validate.js';
+import { render_server_compiled_svelte } from '../utils/compile_svelte.js';
 
 export async function build(files) {
     if (!filled_array(files)) {
@@ -13,7 +14,13 @@ export async function build(files) {
         Logger.info(file);
         const data = read_json(file);
         const content = generate_page_code(data);
-        const result = await compile_server_svelte(content, file);
-        Logger.info(result);
+        const exec_result = await compile_server_svelte(content, file);
+
+        const rendered_result = await render_server_compiled_svelte(exec_result, data, file);
+        // if(rendered_result?.result) {
+        //     Logger.info(file, rendered_result.result);
+        // }
+
+        // Logger.info('result', result);
     }
 }

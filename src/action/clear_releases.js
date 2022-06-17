55fdd399d6485290920a8a1141892ec53035fb39
wyvr <wyvr@daviot.at>
2022-06-17 14:24:59 +0200

    :sparkles: add plugins and make call async

--- src/action/clear_releases.js
+++ src/action/clear_releases.js
@@ -3,11 +3,16 @@ import { join } from 'path';
 import { FOLDER_RELEASES } from '../constants/folder.js';
 import { remove } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
+import { Plugin } from '../utils/plugin.js';
 import { Cwd } from '../vars/cwd.js';
 
-export function clear_releases(keep, ignore_id) {
+export async function clear_releases(keep, ignore_id) {
+    const name = 'clear_releases';
     const path = join(Cwd.get(), FOLDER_RELEASES);
     const releases = readdirSync(path).filter((release) => release != ignore_id);
+
+    await Plugin.before(name, releases, keep, ignore_id);
+
     const delete_releases = releases
         .map((release) => {
             const { ctime } = statSync(join('releases', release), { bigint: true });
@@ -19,6 +24,9 @@ export function clear_releases(keep, ignore_id) {
             remove(join(path, entry.release));
             return entry.release;
         });
+
+    await Plugin.after(name, delete_releases);
+
     Logger.info('keep', keep, 'releases');
     const amount = delete_releases.length;
     if (amount > 0) {

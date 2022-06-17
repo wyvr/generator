0b6a0602338dafae47cb1efa987dbf8b7dd12a82
wyvr <wyvr@daviot.at>
2022-06-17 07:28:10 +0200

    :white_check_mark: improve coverage and test throwing error in storage

--- test/utils/storage/get.test.js
+++ test/utils/storage/get.test.js
@@ -2,8 +2,10 @@ import { deepStrictEqual } from 'assert';
 import { existsSync, rmSync } from 'fs';
 import { describe, it } from 'mocha';
 import { join } from 'path';
+import Sinon from 'sinon';
+import { Logger } from '../../../src/utils/logger.js';
 import { Storage } from '../../../src/utils/storage.js';
-import { to_dirname } from '../../../src/utils/to.js';
+import { to_dirname, to_plain } from '../../../src/utils/to.js';
 import { Cwd } from '../../../src/vars/cwd.js';
 
 describe('utils/storage/get', () => {
@@ -11,12 +13,20 @@ describe('utils/storage/get', () => {
     const __root = join(__dirname, '..', '..', '..');
     const __path = join('test', 'utils', 'storage', '_tests', 'get');
     const test_folder = join(__root, __path);
+    let log = [];
 
     before(async () => {
         Cwd.set(__root);
         Storage.set_location(__path);
         await Storage.open('test_get');
         await Storage.cache['test_get'].run('INSERT INTO "data" (key, value) VALUES (?, ?);', 'key', '"value"');
+        Sinon.stub(Logger, 'output');
+        Logger.output.callsFake((...msg) => {
+            log.push(msg.map(to_plain));
+        });
+    });
+    afterEach(() => {
+        log = [];
     });
     after(() => {
         Storage.location = undefined;
@@ -25,20 +35,39 @@ describe('utils/storage/get', () => {
             rmSync(test_folder, { recursive: true, force: true });
         }
         Storage.cache = {};
+        Logger.output.restore();
     });
     it('undefined', async () => {
         deepStrictEqual(await Storage.get(), undefined);
+        deepStrictEqual(log, []);
     });
     it('missing key or data', async () => {
         deepStrictEqual(await Storage.get('test_get'), undefined);
+        deepStrictEqual(log, []);
     });
     it('get value', async () => {
         deepStrictEqual(await Storage.get('test_get', 'key'), 'value');
+        deepStrictEqual(log, []);
     });
     it('unknown key', async () => {
         deepStrictEqual(await Storage.get('test_get', 'unknown'), undefined);
+        deepStrictEqual(log, []);
     });
     it('unknown database', async () => {
         deepStrictEqual(await Storage.get('test_get_unknown', 'unknown'), undefined);
+        deepStrictEqual(log, []);
+    });
+    it('throw error', async () => {
+        await Storage.open('test_get_error');
+        await Storage.cache['test_get_error'].run('DROP TABLE "data";');
+        deepStrictEqual(await Storage.get('test_get_error', 'key'), undefined);
+        deepStrictEqual(log, [
+            [
+                '',
+                '',
+                '✖',
+                '@storage\n' + '[Error] SQLITE_ERROR: no such table: data\n' + 'stack\n' + 'source test_get_error',
+            ],
+        ]);
     });
 });

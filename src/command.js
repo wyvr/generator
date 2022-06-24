d5d62f737faaeecb19218491524c9f0de8527f7f
wyvr <wyvr@daviot.at>
2022-06-24 09:52:18 +0200

    :loud_sound: add total execution time

--- src/command.js
+++ src/command.js
@@ -7,22 +7,27 @@ import { build_command } from './command/build.js';
 // import { regenerate_command } from './command/regenerate';
 // import { report_command } from './command/report';
 import { unknown_command } from './command/unknown.js';
+import { nano_to_milli } from './utils/convert.js';
+import { Logger } from './utils/logger.js';
 import { filled_array } from './utils/validate.js';
 
 export function get_command(config) {
     const commands = config?.cli?.command;
-    if(!filled_array(commands)) {
+    if (!filled_array(commands)) {
         return undefined;
     }
     return commands[0];
 }
 
 export async function command(config) {
+    const start = process.hrtime.bigint();
+    let result = '';
     switch (get_command(config)) {
         // case 'app':
         //     return await app_command(config);
         case 'build':
-            return await build_command(config);
+            result = await build_command(config);
+            break;
         // case 'clear':
         //     return await clear_command(config);
         // case 'create':
@@ -36,6 +41,10 @@ export async function command(config) {
         // case 'report':
         //     return await report_command(config);
         default:
-            return await unknown_command(config);
+            result = await unknown_command(config);
+            break;
     }
+    const duration_text = `${Math.round(nano_to_milli(process.hrtime.bigint() - start))} ${Logger.color.dim('ms')}`;
+    Logger.success('total execution time', duration_text);
+    return result;
 }

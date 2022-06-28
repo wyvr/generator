5c9e68533bb57d2bfd56005887623ad59b339fd4
wyvr <wyvr@daviot.at>
2022-06-28 09:25:00 +0200

    :white_check_mark: fix process exit and changed messages

--- test/presentation/package_report/package_report.test.js
+++ test/presentation/package_report/package_report.test.js
@@ -1,10 +1,13 @@
 import { strictEqual, deepStrictEqual } from 'assert';
 import { describe, it } from 'mocha';
+import Sinon from 'sinon';
 import { package_report } from '../../../src/presentation/package_report.js';
+import { to_plain } from '../../../src/utils/to.js';
 
 describe('presentation/package_report/package_report', () => {
     let log, err;
     let result = [];
+    let exit_value;
     const base_report = {
         success: true,
         info: [],
@@ -16,41 +19,48 @@ describe('presentation/package_report/package_report', () => {
         // runs once before the first test in this block
         log = console.log;
         console.log = (...args) => {
-            result.push(args);
+            result.push(args.map(to_plain));
         };
         err = console.error;
         console.error = (...args) => {
-            result.push(args);
+            result.push(args.map(to_plain));
         };
+        Sinon.stub(process, 'exit');
+        process.exit.callsFake((code) => {
+            exit_value = code;
+        });
     });
     afterEach(() => {
         result = [];
+        exit_value = undefined;
     });
     after(() => {
         // runs once after the last test in this block
         console.log = log;
         console.error = err;
+        process.exit.restore();
     });
     it('missing packages', () => {
         package_report();
-        deepStrictEqual(result, [['\u001b[33m⚠\u001b[39m', '\u001b[33mno packages active\u001b[39m']]);
+        deepStrictEqual(result, [['✖', 'no packages active']]);
+        deepStrictEqual(exit_value, 1);
     });
     it('available packages', () => {
         package_report([{ name: 'name', path: 'path' }]);
-        deepStrictEqual(result, [['\x1B[2m›\x1B[22m', 'packages \x1B[32mname\x1B[39m']]);
+        deepStrictEqual(result, [['›', 'packages name']]);
     });
     it('disabled packages', () => {
         package_report([], [{ name: 'name', path: 'path' }]);
         deepStrictEqual(result, [
-            ['\x1B[33m⚠\x1B[39m', '\x1B[33mno packages active\x1B[39m'],
-            ['\x1B[33m⚠\x1B[39m', '\x1B[33mdisabled packages name\x1B[39m'],
+            ['✖', 'no packages active'],
+            ['⚠', 'disabled packages name'],
         ]);
     });
     it('disabled packages', () => {
         package_report(undefined, [{ name: 'name', path: 'path' }]);
         deepStrictEqual(result, [
-            ['\x1B[33m⚠\x1B[39m', '\x1B[33mno packages active\x1B[39m'],
-            ['\x1B[33m⚠\x1B[39m', '\x1B[33mdisabled packages name\x1B[39m'],
+            ['✖', 'no packages active'],
+            ['⚠', 'disabled packages name'],
         ]);
     });
 });

bf6e20827b321d75504d99745568654545a197f1
wyvr <wyvr@daviot.at>
2022-06-30 13:22:17 +0200

    :bug: remove uninstalled dependency

--- test/action/publish.test.js
+++ test/action/publish.test.js
@@ -1,13 +1,12 @@
 import { deepStrictEqual } from 'assert';
-import { existsSync, mkdirSync, rmSync } from 'fs';
-import { remove } from 'fs-extra';
 import { describe, it } from 'mocha';
 import { join } from 'path';
 import Sinon from 'sinon';
 import { publish } from '../../src/action/publish.js';
 import { FOLDER_RELEASES } from '../../src/constants/folder.js';
+import { remove } from '../../src/utils/file.js';
 import { Logger } from '../../src/utils/logger.js';
-import { to_dirname, to_plain } from '../../src/utils/to.js';
+import { to_plain } from '../../src/utils/to.js';
 import { Cwd } from '../../src/vars/cwd.js';
 import { ReleasePath } from '../../src/vars/release_path.js';
 import { UniqId } from '../../src/vars/uniq_id.js';

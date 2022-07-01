ce25c546495b9c49975797de3ee3afe6bb7872d2
wyvr <wyvr@daviot.at>
2022-07-01 11:41:30 +0200

    :white_check_mark: increase coverage

--- test/utils/i18n/write_language.test.js
+++ test/utils/i18n/write_language.test.js
@@ -3,17 +3,28 @@ import { describe, it } from 'mocha';
 import { write_language } from '../../../src/utils/i18n.js';
 import { Cwd } from '../../../src/vars/cwd.js';
 import { to_dirname } from '../../../src/utils/to.js';
+import { read, remove } from '../../../src/utils/file.js';
+import { join } from 'path';
 
 describe('action/i18n/write_language', () => {
-    const cwd = Cwd.get();
+    const __dirname = join(to_dirname(import.meta.url), '_tests');
 
-    const __dirname = to_dirname(import.meta.url);
-
-    afterEach(() => {
-        Cwd.set(cwd);
+    before(() => {
+        Cwd.set(__dirname);
+    });
+    after(() => {
+        Cwd.set(undefined);
     });
     it('undefined', () => {
         const result = write_language();
-       strictEqual(result, false);
+        strictEqual(result, false);
+    });
+    it('undefined', () => {
+        const result = write_language('ES', { test: 'test' });
+        const file = join(__dirname, 'gen', 'i18n', 'es.json');
+        const content = read(file);
+        remove(file);
+        strictEqual(result, true);
+        strictEqual(content, '{"test":"test"}');
     });
 });

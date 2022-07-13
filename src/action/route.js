1b673b354063bbaa378deb3a046bf8ba9935a774
wyvr <wyvr@daviot.at>
2022-07-13 17:21:03 +0200

    :sparkles: add collections

--- src/action/route.js
+++ src/action/route.js
@@ -4,6 +4,8 @@ import { Event } from '../utils/event.js';
 import { Logger } from '../utils/logger.js';
 import { Plugin } from '../utils/plugin.js';
 import { collect_routes } from '../utils/route.js';
+import { Storage } from '../utils/storage.js';
+import { filled_string } from '../utils/validate.js';
 import { WorkerController } from '../worker/controller.js';
 import { measure_action } from './helper.js';
 
@@ -11,14 +13,30 @@ export async function routes(package_tree) {
     const name = 'route';
     const identifier_name = get_name(WorkerEmit.identifier);
     const identifiers = {};
+    const collection_name = get_name(WorkerEmit.collection);
+    const collections = {};
 
     await measure_action(name, async () => {
-        const listener_id = Event.on('emit', identifier_name, (data) => {
+        const identifier_id = Event.on('emit', identifier_name, (data) => {
             if (!data) {
                 return;
             }
             delete data.type;
             identifiers[data.identifier] = data;
+        });
+        const collection_id = Event.on('emit', collection_name, (data) => {
+            if (!data || !data.collection) {
+                return;
+            }
+            data.collection.forEach((entry) => {
+                if (!filled_string(entry.url)) {
+                    return;
+                }
+                if (!collections[entry.scope]) {
+                    collections[entry.scope] = [];
+                }
+                collections[entry.scope].push(entry);
+            });
         });
 
         const data = collect_routes(undefined, package_tree);
@@ -30,7 +48,8 @@ export async function routes(package_tree) {
         });
 
         // remove listeners
-        Event.off('emit', identifier_name, listener_id);
+        Event.off('emit', identifier_name, identifier_id);
+        Event.off('emit', collection_name, collection_id);
 
         const identifier_length = Object.keys(identifiers).length;
         Logger.info(
@@ -39,6 +58,22 @@ export async function routes(package_tree) {
             identifier_length == 1 ? 'identifier' : 'identifiers',
             Logger.color.dim('different layout combinations')
         );
+
+        // sort the collection entries
+        Object.keys(collections).forEach((key) => {
+            collections[key] = collections[key]
+                .sort((a, b) => a.url.localeCompare(b.url))
+                .sort((a, b) => {
+                    if (a.order > b.order) {
+                        return -1;
+                    }
+                    if (a.order < b.order) {
+                        return 1;
+                    }
+                    return 0;
+                });
+        });
+        await Storage.set('collection', collections);
     });
 
     return identifiers;
--- src/worker_action/route.js
+++ src/worker_action/route.js
@@ -1,18 +1,34 @@
+import { WorkerAction } from '../struc/worker_action.js';
+import { WorkerEmit } from '../struc/worker_emit.js';
 import { execute_route, write_routes } from '../utils/route.js';
 import { filled_array, is_null } from '../utils/validate.js';
+import { send_action } from '../worker/communication.js';
 import { process_page_data } from './process_page_data.js';
 
 export async function route(files) {
     if (!filled_array(files)) {
         return;
     }
-
+    const collections = [];
     for (const route of files) {
         const wyvr_pages = await execute_route(route);
         if (is_null(wyvr_pages)) {
             continue;
         }
-        const processed_pages = wyvr_pages.map((wyvr_page) => process_page_data(wyvr_page));
+        const processed_pages = wyvr_pages.map((wyvr_page) => {
+            const page = process_page_data(wyvr_page);
+            if (page._wyvr.collection) {
+                collections.push(...page._wyvr.collection);
+            }
+            return page;
+        });
         write_routes(processed_pages);
+    }
+    if (filled_array(collections)) {
+        const collection_emit = {
+            type: WorkerEmit.collection,
+            collection: collections,
+        };
+        send_action(WorkerAction.emit, collection_emit);
     }
 }

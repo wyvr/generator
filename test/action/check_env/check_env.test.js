2bf414015406c864f53dc1f17fbacc0e9ef8bc7d
wyvr <wyvr@daviot.at>
2022-07-20 06:24:06 +0200

    :white_check_mark: fix broken tests and expand coverage

--- test/action/check_env/check_env.test.js
+++ test/action/check_env/check_env.test.js
@@ -4,34 +4,79 @@ import { join } from 'path';
 import { Cwd } from '../../../src/vars/cwd.js';
 import { check_env } from '../../../src/action/check_env.js';
 import { ERRORS } from '../../../src/constants/errors.js';
-import { to_dirname } from '../../../src/utils/to.js';
+import { to_dirname, to_plain } from '../../../src/utils/to.js';
+import Sinon from 'sinon';
 
 describe('action/check_env/check_env', () => {
     const cwd = Cwd.get();
     const __dirname = to_dirname(import.meta.url);
     const __root = join(__dirname, '..', '..', '..');
 
-    afterEach(() => {
+    let exit_value;
+    let log = [];
+    let sandbox;
+    before(() => {
+        sandbox = Sinon.createSandbox();
+        sandbox.stub(process, 'exit');
+        process.exit.callsFake((code) => {
+            exit_value = code;
+        });
+        sandbox.stub(console, 'log');
+        console.log.callsFake((...args) => {
+            log.push(args.map(to_plain));
+        });
+        sandbox.stub(console, 'error');
+        console.error.callsFake((...args) => {
+            log.push(args.map(to_plain));
+        });
         Cwd.set(cwd);
     });
+    afterEach(() => {
+        exit_value = undefined;
+        log = [];
+    });
+    after(() => {
+        sandbox.restore();
+        Cwd.set(undefined);
+    });
+
     it('error run in same folder', async () => {
         Cwd.set(__root);
-        const report = await check_env();
-        strictEqual(report.error.indexOf(ERRORS.run_in_same_folder) > -1, true);
+        await check_env();
+        deepStrictEqual(log, [
+            ['✖', 'current directory is wyvr root folder, please start wyvr in another directory'],
+            ['✖', 'terminated wyvr because of critical errors'],
+        ]);
+        strictEqual(exit_value, 1);
     });
     it('missing package.json', async () => {
         Cwd.set(join(__dirname, '_tests', 'empty'));
-        const report = await check_env();
-        strictEqual(report.warning.indexOf(ERRORS.package_is_not_present) > -1, true);
+        await check_env();
+        deepStrictEqual(log, [
+            ['⚠', 'package.json is not present'],
+            ['✖', 'wyvr.js is not present'],
+            ['✖', 'terminated wyvr because of critical errors'],
+        ]);
+        strictEqual(exit_value, 1);
     });
     it('invalid package.json', async () => {
         Cwd.set(join(__dirname, '_tests', 'invalid'));
-        const report = await check_env();
-        strictEqual(report.warning.indexOf(ERRORS.package_is_not_valid) > -1, true);
+        await check_env();
+        deepStrictEqual(log, [
+            ['⚠', 'package.json is invalid JSON'],
+            ['✖', 'wyvr.js is not present'],
+            ['✖', 'terminated wyvr because of critical errors'],
+        ]);
+        strictEqual(exit_value, 1);
     });
     it('missing wyvr.js', async () => {
         Cwd.set(join(__dirname, '_tests', 'empty'));
-        const report = await check_env();
-        strictEqual(report.error.indexOf(ERRORS.wyvr_js_is_not_present) > -1, true);
+        await check_env();
+        deepStrictEqual(log, [
+            ['⚠', 'package.json is not present'],
+            ['✖', 'wyvr.js is not present'],
+            ['✖', 'terminated wyvr because of critical errors'],
+        ]);
+        strictEqual(exit_value, 1);
     });
 });
---
+++ test/action/check_env/get_report.test.js
@@ -0,0 +1,45 @@
+import { strictEqual, deepStrictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { join } from 'path';
+import { Cwd } from '../../../src/vars/cwd.js';
+import { get_report } from '../../../src/action/check_env.js';
+import { ERRORS } from '../../../src/constants/errors.js';
+import { to_dirname } from '../../../src/utils/to.js';
+
+describe('action/check_env/get_report', () => {
+    const cwd = Cwd.get();
+    const __dirname = to_dirname(import.meta.url);
+    const __root = join(__dirname, '..', '..', '..');
+
+    afterEach(() => {
+        Cwd.set(cwd);
+    });
+    after(() => {
+        Cwd.set(undefined);
+    });
+    it('every', async () => {
+        Cwd.set(join(__dirname, '_tests', 'simple'));
+        const report = await get_report();
+        deepStrictEqual(report.error, []);
+    });
+    it('error run in same folder', async () => {
+        Cwd.set(__root);
+        const report = await get_report();
+        strictEqual(report.error.indexOf(ERRORS.run_in_same_folder) > -1, true);
+    });
+    it('missing package.json', async () => {
+        Cwd.set(join(__dirname, '_tests', 'empty'));
+        const report = await get_report();
+        strictEqual(report.warning.indexOf(ERRORS.package_is_not_present) > -1, true);
+    });
+    it('invalid package.json', async () => {
+        Cwd.set(join(__dirname, '_tests', 'invalid'));
+        const report = await get_report();
+        strictEqual(report.warning.indexOf(ERRORS.package_is_not_valid) > -1, true);
+    });
+    it('missing wyvr.js', async () => {
+        Cwd.set(join(__dirname, '_tests', 'empty'));
+        const report = await get_report();
+        strictEqual(report.error.indexOf(ERRORS.wyvr_js_is_not_present) > -1, true);
+    });
+});

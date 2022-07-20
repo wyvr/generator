3dc2b32a8a89a6574aaef8180b8f7ec3805f7aff
wyvr <wyvr@daviot.at>
2022-07-20 06:31:16 +0200

    :white_check_mark: test get_critical_css

--- src/utils/critical.js
+++ src/utils/critical.js
@@ -2,24 +2,30 @@ import critical from 'critical';
 import { ReleasePath } from '../vars/release_path.js';
 import { get_error_message } from './error.js';
 import { Logger } from './logger.js';
+import { filled_string } from './validate.js';
 
 export async function get_critical_css(content, file) {
     let css = '';
+    if (!filled_string(content)) {
+        return css;
+    }
     try {
         // create above the fold inline css
-        const result = await critical.generate({
-            inline: false, // generates CSS
-            base: ReleasePath.get(),
-            html: content,
-            dimensions: [
-                { width: 320, height: 568 },
-                { width: 360, height: 720 },
-                { width: 480, height: 800 },
-                { width: 1024, height: 768 },
-                { width: 1280, height: 1024 },
-                { width: 1920, height: 1080 },
-            ],
-        });
+        const result = await critical
+            .generate({
+                inline: false, // generates CSS
+                base: ReleasePath.get(),
+                html: content,
+                dimensions: [
+                    { width: 320, height: 568 },
+                    { width: 360, height: 720 },
+                    { width: 480, height: 800 },
+                    { width: 1024, height: 768 },
+                    { width: 1280, height: 1024 },
+                    { width: 1920, height: 1080 },
+                ],
+                rebase: undefined
+            });
         css = result.css;
     } catch (e) {
         Logger.error(get_error_message(e, file, 'critical'));
---
+++ test/utils/critical/get_critical_css.test.js
@@ -0,0 +1,76 @@
+import { deepStrictEqual } from 'assert';
+import { readdirSync } from 'fs';
+import { describe } from 'mocha';
+import { join } from 'path';
+import { get_critical_css } from '../../../src/utils/critical.js';
+import { remove } from '../../../src/utils/file.js';
+import { to_dirname, to_plain } from '../../../src/utils/to.js';
+import { Cwd } from '../../../src/vars/cwd.js';
+import { ReleasePath } from '../../../src/vars/release_path.js';
+
+describe('utils/css/get_critical_css', () => {
+    let log = [];
+    let console_error, console_log, write;
+    const cwd = process.cwd();
+    const __dirname = join(to_dirname(import.meta.url), '_tests');
+    before(() => {
+        Cwd.set(__dirname);
+        console_error = console.error;
+        console.error = (...values) => {
+            log.push(values.map(to_plain));
+        };
+        console_log = console.log;
+        console.log = (...values) => {
+            log.push(values.map(to_plain));
+        };
+        write = process.stderr.write;
+        process.stderr.write = (value) => {
+            log.push(to_plain(value));
+        };
+
+        ReleasePath.set(__dirname);
+    });
+    afterEach(() => {
+        log = [];
+        readdirSync(__dirname).forEach((file) => {
+            remove(join(__dirname, file));
+        });
+    });
+    after(() => {
+        console.error = console_error;
+        console.log = console_log;
+        process.stderr.write = write;
+        Cwd.set(undefined);
+        ReleasePath.set(undefined);
+    });
+
+    it('undefined', async () => {
+        deepStrictEqual(await get_critical_css(), '');
+        deepStrictEqual(log, []);
+    });
+    it('empty string', async () => {
+        deepStrictEqual(await get_critical_css(''), '');
+        deepStrictEqual(log, []);
+    });
+    it('base html', async () => {
+        deepStrictEqual(
+            await get_critical_css(
+                '<html><head><style>a {color:red}</style></head><body><a href="#">test</a></body></html>'
+            ),
+            'a{color:red}'
+        );
+        deepStrictEqual(log, ['Not rebasing assets for a {color:red}. Use "rebase" option\n']);
+    });
+    it('missing file', async () => {
+        deepStrictEqual(
+            await get_critical_css(
+                '<html><head><link rel="stylesheet" href="test.css"></head><body><a href="#">test</a></body></html>'
+            ),
+            ''
+        );
+        deepStrictEqual(log, [['✖', '@critical\n[Error] Error: File not found: test.css']]);
+    });
+    it('no media query and no file', async () => {
+        deepStrictEqual(await get_critical_css('a {color: red;}'), '');
+    });
+});

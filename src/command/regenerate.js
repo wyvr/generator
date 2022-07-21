8d478750eb36874a2e813e53dd40e7f73b90823c
wyvr <wyvr@daviot.at>
2022-07-21 15:01:30 +0200

    :construction: add building of changed routes and extract identifiers

--- src/command/regenerate.js
+++ src/command/regenerate.js
@@ -1,13 +1,16 @@
 import { join } from 'path';
 import { copy_files, copy_folder } from '../action/copy.js';
+import { measure_action } from '../action/helper.js';
 import { i18n } from '../action/i18n.js';
 import { FOLDER_ASSETS, FOLDER_GEN, FOLDER_I18N, FOLDER_ROUTES } from '../constants/folder.js';
+import { Route } from '../model/route.js';
 import { WorkerAction } from '../struc/worker_action.js';
+import { get_name, WorkerEmit } from '../struc/worker_emit.js';
 import { Config } from '../utils/config.js';
+import { Event } from '../utils/event.js';
 import { remove } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
-import { to_relative_path } from '../utils/to.js';
-import { in_array } from '../utils/validate.js';
+import { filled_array, filled_string, in_array } from '../utils/validate.js';
 import { Cwd } from '../vars/cwd.js';
 import { ReleasePath } from '../vars/release_path.js';
 import { WorkerController } from '../worker/controller.js';
@@ -24,61 +27,114 @@ export async function regenerate_command(changed_files) {
     Logger.info('packages', packages);
     const gen_folder = join(Cwd.get(), FOLDER_GEN);
 
-    // regenerate assets
-    if (in_array(fragments, FOLDER_ASSETS)) {
-        const assets = frag_files.assets;
-        // copy modified and added files into the release and gen folder
-        if (assets.change || assets.add) {
-            const mod_assets = []
-                .concat(assets.change || [], assets.add || [])
-                .map((file) => ({ src: file.path, target: '.' + file.rel_path }));
-            copy_files(mod_assets, ReleasePath.get());
-            copy_files(mod_assets, gen_folder);
+    await measure_action('regenerate', async () => {
+        // regenerate assets
+        if (in_array(fragments, FOLDER_ASSETS)) {
+            const assets = frag_files.assets;
+            // copy modified and added files into the release and gen folder
+            if (assets.change || assets.add) {
+                const mod_assets = []
+                    .concat(assets.change || [], assets.add || [])
+                    .map((file) => ({ src: file.path, target: '.' + file.rel_path }));
+                copy_files(mod_assets, ReleasePath.get());
+                copy_files(mod_assets, gen_folder);
+            }
+            if (assets.unlink) {
+                assets.unlink.forEach((file) => {
+                    remove(join(ReleasePath.get(), file.rel_path));
+                    remove(join(gen_folder, file.rel_path));
+                });
+            }
+            // @TODO reload resource in browser
         }
-        if (assets.unlink) {
-            assets.unlink.forEach((file) => {
-                remove(join(ReleasePath.get(), file.rel_path));
-                remove(join(gen_folder, file.rel_path));
+
+        // regenerate i18n
+        if (in_array(fragments, FOLDER_I18N)) {
+            await i18n(packages);
+            copy_folder(join(Cwd.get(), FOLDER_GEN), [FOLDER_I18N], ReleasePath.get());
+            // @TODO reload the whole browser page
+        }
+
+        // regenerate routes
+        let routes = [];
+        const identifiers = {};
+        const collections = {};
+        if (in_array(fragments, FOLDER_ROUTES)) {
+            if (frag_files.routes.change || frag_files.routes.add) {
+                const mod_routes = [].concat(frag_files.routes.change || [], frag_files.routes.add || []);
+                const mod_routes_copy = mod_routes.map((file) => ({ src: file.path, target: '.' + file.rel_path }));
+                copy_files(mod_routes_copy, gen_folder);
+                const routes_data = mod_routes.map((file) => {
+                    return new Route({
+                        path: join(gen_folder, file.rel_path),
+                        rel_path: file.rel_path.replace(/^\//, ''),
+                        pkg: file.pkg,
+                    });
+                });
+                const identifier_name = get_name(WorkerEmit.identifier);
+                const collection_name = get_name(WorkerEmit.collection);
+                const routes_name = get_name(WorkerEmit.route);
+                const identifier_id = Event.on('emit', identifier_name, (data) => {
+                    if (!data) {
+                        return;
+                    }
+                    delete data.type;
+                    identifiers[data.identifier] = data;
+                });
+                const collection_id = Event.on('emit', collection_name, (data) => {
+                    if (!data || !data.collection) {
+                        return;
+                    }
+                    data.collection.forEach((entry) => {
+                        if (!filled_string(entry.url)) {
+                            return;
+                        }
+                        if (!collections[entry.scope]) {
+                            collections[entry.scope] = [];
+                        }
+                        collections[entry.scope].push(entry);
+                    });
+                });
+                const routes_id = Event.on('emit', routes_name, (data) => {
+                    if (data && data.routes) {
+                        routes.push(...data.routes);
+                    }
+                });
+                await WorkerController.process_in_workers(WorkerAction.route, routes_data, 10);
+
+                // remove listeners
+                Event.off('emit', identifier_name, identifier_id);
+                Event.off('emit', collection_name, collection_id);
+                Event.off('emit', routes_name, routes_id);
+            }
+            // @TODO reload the whole browser page
+        }
+
+        if(filled_array(routes)) {
+            const identifier_name = get_name(WorkerEmit.identifier);
+            const identifier_id = Event.on('emit', identifier_name, (data) => {
+                if (!data) {
+                    return;
+                }
+                delete data.type;
+                identifiers[data.identifier] = data;
             });
+            await WorkerController.process_in_workers(WorkerAction.build, routes, 100);
+            Event.off('emit', identifier_name, identifier_id);
         }
-        // @TODO reload resource in browser
-    }
 
-    // regenerate i18n
-    if (in_array(fragments, FOLDER_I18N)) {
-        await i18n(packages);
-        copy_folder(join(Cwd.get(), FOLDER_GEN), [FOLDER_I18N], ReleasePath.get());
-        // @TODO reload the whole browser page
-    }
+        Logger.info('identifiers', identifiers);
 
-    // regenerate routes
-    if (in_array(fragments, FOLDER_ROUTES)) {
-        if (frag_files.routes.change || frag_files.routes.add) {
-            const mod_routes = [].concat(frag_files.routes.change || [], frag_files.routes.add || []);
-            const mod_routes_copy = mod_routes.map((file) => ({ src: file.path, target: '.' + file.rel_path }));
-            copy_files(mod_routes_copy, gen_folder);
-            const routes_data = mod_routes.map((file) => {
-                return {
-                    path: join(gen_folder, file.rel_path),
-                    rel_path: file.rel_path.replace(/^\//, ''),
-                    pkg: file.pkg,
-                };
-            });
-            Logger.info('routes data', routes_data);
-            await WorkerController.process_in_workers(WorkerAction.route, routes_data, 10);
-        }
-        // @TODO reload the whole browser page
-    }
-
-    // Object.keys(changed_files).forEach((event) => {
-    //     changed_files[event].forEach((file) => {
-    //         switch (event) {
-    //             case 'unlink':
-    //                 remove_deleted_files(file.rel_path);
-    //                 break;
-    //         }
-    //     });
-    // });
+        // Object.keys(changed_files).forEach((event) => {
+        //     changed_files[event].forEach((file) => {
+        //         switch (event) {
+        //             case 'unlink':
+        //                 remove_deleted_files(file.rel_path);
+        //                 break;
+        //         }
+        //     });
+        // });
+    });
 
     return;
 }
@@ -98,10 +154,6 @@ export function split_changed_files_by_fragment(changed_files) {
         });
     });
     return result;
-}
-
-function remove_deleted_files(rel_path) {
-    remove(join(Cwd.get(), FOLDER_GEN, rel_path));
 }
 
 // function find_package_of_file(file) {

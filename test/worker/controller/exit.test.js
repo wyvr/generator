8c7dfcc2a76579a05f0808fafd08aaf65e7c9653
wyvr <wyvr@daviot.at>
2022-07-22 19:00:41 +0200

    :white_check_mark: test exit workers

---
+++ test/worker/controller/exit.test.js
@@ -0,0 +1,32 @@
+import { deepStrictEqual, strictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import Sinon from 'sinon';
+import { to_plain } from '../../../src/utils/to.js';
+import { WorkerController } from '../../../src/worker/controller.js';
+
+describe('worker/controller/exit', () => {
+    let logger_messages = [];
+
+    before(() => {
+        Sinon.stub(console, 'error');
+        console.error.callsFake((...msg) => {
+            logger_messages.push(to_plain(msg));
+        });
+    });
+    beforeEach(() => {
+        WorkerController.workers = [];
+    });
+    afterEach(() => {
+        WorkerController.workers = [];
+        logger_messages = [];
+    });
+    after(() => {
+        console.error.restore();
+    });
+    it('create worker', () => {
+        WorkerController.create_workers(1);
+        WorkerController.exit();
+        strictEqual(WorkerController.workers.length, 1);
+        deepStrictEqual(logger_messages, []);
+    });
+});

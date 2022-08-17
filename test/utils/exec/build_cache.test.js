ad61a6775644a73ef599a9b44d44a5010affb5a9
wyvr <wyvr@daviot.at>
2022-08-17 09:54:43 +0200

    :fire: delete cache files

--- test/utils/exec/build_cache.test.js
+++ test/utils/exec/build_cache.test.js
@@ -3,6 +3,7 @@ import { describe, it } from 'mocha';
 import { join } from 'path';
 import Sinon from 'sinon';
 import { build_cache } from '../../../src/utils/exec.js';
+import { collect_files, remove } from '../../../src/utils/file.js';
 import { to_plain } from '../../../src/utils/to.js';
 import { Cwd } from '../../../src/vars/cwd.js';
 
@@ -23,15 +24,20 @@ describe('utils/exec/build_cache', () => {
     after(() => {
         Cwd.set(undefined);
         sandbox.restore();
+        collect_files(dir, 'json').forEach((file) => {
+            if (file.match(/exec_cache.json$/)) {
+                remove(join(dir, file));
+            }
+        });
     });
     it('empty gen/exec folder', async () => {
         Cwd.set(join(dir, 'empty'));
         deepStrictEqual(await build_cache(), []);
-        deepStrictEqual(log, [])
+        deepStrictEqual(log, []);
     });
     it('build cache', async () => {
         Cwd.set(join(dir, 'simple'));
         deepStrictEqual(await build_cache(), [join(dir, 'simple/gen/exec/a.js')]);
-        deepStrictEqual(log, [])
+        deepStrictEqual(log, []);
     });
 });

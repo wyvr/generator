6adf5f2aa8bc02627195af08c57f7b121176532c
wyvr <wyvr@daviot.at>
2022-08-19 13:44:22 +0200

    :white_check_mark: test prepare_code_to_compile

---
+++ test/utils/compile_svelte/prepare_code_to_compile.test.js
@@ -0,0 +1,67 @@
+import { deepStrictEqual, strictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { prepare_code_to_compile } from '../../../src/utils/compile_svelte.js';
+import { Cwd } from '../../../src/vars/cwd.js';
+import { join } from 'path';
+import { to_plain } from '../../../src/utils/to.js';
+import Sinon from 'sinon';
+import { Logger } from '../../../src/utils/logger.js';
+import { read } from '../../../src/utils/file.js';
+import { Env } from '../../../src/vars/env.js';
+import { EnvType } from '../../../src/struc/env.js';
+
+describe('utils/compile_svelte/prepare_code_to_compile', () => {
+    let log = [];
+    let send_data;
+    let mock_send;
+    const path = join(process.cwd(), 'test', 'utils', 'compile_svelte', '_tests');
+    before(() => {
+        Cwd.set(path);
+        Sinon.stub(Logger, 'output');
+        Logger.output.callsFake((...msg) => {
+            log.push(msg.map(to_plain));
+        });
+        mock_send = process.send;
+        process.send = (data) => {
+            send_data = data;
+        };
+    });
+    beforeEach(() => {});
+    afterEach(() => {
+        log = [];
+    });
+    after(() => {
+        Cwd.set(undefined);
+        Logger.output.restore();
+        process.send = mock_send;
+    });
+
+    it('undefined', async () => {
+        deepStrictEqual(await prepare_code_to_compile(), undefined);
+    });
+    it('simple', async () => {
+        deepStrictEqual(await prepare_code_to_compile('<p>Hello</p>', 'file', 'server'), '<p>Hello</p>');
+    });
+    it('import', async () => {
+        deepStrictEqual(
+            await prepare_code_to_compile(
+                '<script>import Header from "@src/component/Header.svelte";</script> <Header />',
+                'file',
+                'server'
+            ),
+            "<script>import Header from '/home/p/wyvr/generator/test/utils/compile_svelte/_tests/gen/server/component/Header.js';</script> <Header />"
+        );
+    });
+    it('import dev', async () => {
+        Env.set(EnvType.debug);
+        const result = await prepare_code_to_compile(
+            '<script>import Header from "@src/component/Header.svelte";</script> <Header />',
+            'file',
+            'server'
+        );
+        Env.value = undefined;
+        strictEqual(
+            result.indexOf("<script>import Header from '/home/p/wyvr/generator/test/utils/compile_svelte/_tests/gen/server/component/Header.js?") > -1, true, 'Doesnt contain cache breaker'
+        );
+    });
+});

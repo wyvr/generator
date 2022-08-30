85e4734c52601757713e00db2a443aa53449b645
wyvr <wyvr@daviot.at>
2022-08-30 14:01:19 +0200

    :white_check_mark: test hashes

---
+++ test/utils/hash/get_file_hash.test.js
@@ -0,0 +1,31 @@
+import { deepStrictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { join } from 'path';
+import { get_file_hash } from '../../../src/utils/hash.js';
+import { Cwd } from '../../../src/vars/cwd.js';
+
+describe('utils/hash/get_file_hash', () => {
+    const path = join(process.cwd(), 'test', 'utils', 'hash', '_tests');
+
+    before(async () => {
+        Cwd.set(path);
+    });
+    beforeEach(() => {});
+    afterEach(() => {});
+    after(() => {
+        Cwd.set(undefined);
+    });
+    it('undefined', () => {
+        const result = get_file_hash();
+        deepStrictEqual(result, undefined);
+    });
+    it('non existing files', () => {
+        const result = get_file_hash('non.txt');
+        deepStrictEqual(result, undefined);
+    });
+    it('existing files', () => {
+        const file_path = join(path, 'test.txt');
+        const result = get_file_hash(file_path);
+        deepStrictEqual(result, '0f24276d');
+    });
+});
---
+++ test/utils/hash/get_file_time_hash.test.js
@@ -0,0 +1,31 @@
+import { deepStrictEqual } from 'assert';
+import { describe, it } from 'mocha';
+import { join } from 'path';
+import { get_file_time_hash } from '../../../src/utils/hash.js';
+import { Cwd } from '../../../src/vars/cwd.js';
+
+describe('utils/hash/get_file_time_hash', () => {
+    const path = join(process.cwd(), 'test', 'utils', 'hash', '_tests');
+
+    before(async () => {
+        Cwd.set(path);
+    });
+    beforeEach(() => {});
+    afterEach(() => {});
+    after(() => {
+        Cwd.set(undefined);
+    });
+    it('undefined', () => {
+        const result = get_file_time_hash();
+        deepStrictEqual(result, undefined);
+    });
+    it('non existing files', () => {
+        const result = get_file_time_hash('non.txt');
+        deepStrictEqual(result, undefined);
+    });
+    it('existing files', () => {
+        const file_path = join(path, 'test.txt');
+        const result = get_file_time_hash(file_path);
+        deepStrictEqual(result.length, 17);
+    });
+});

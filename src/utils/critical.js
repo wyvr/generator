bd3b52c2a0a73cfd25cab387ee9f8706d0d629ec
wyvr <wyvr@daviot.at>
2022-09-19 08:52:35 +0200

    :package: update dependencies

--- src/utils/critical.js
+++ src/utils/critical.js
@@ -1,4 +1,4 @@
-import critical from 'critical';
+import { generate } from 'critical';
 import { ReleasePath } from '../vars/release_path.js';
 import { get_error_message } from './error.js';
 import { Logger } from './logger.js';
@@ -11,21 +11,20 @@ export async function get_critical_css(content, file) {
     }
     try {
         // create above the fold inline css
-        const result = await critical
-            .generate({
-                inline: false, // generates CSS
-                base: ReleasePath.get(),
-                html: content,
-                dimensions: [
-                    { width: 320, height: 568 },
-                    { width: 360, height: 720 },
-                    { width: 480, height: 800 },
-                    { width: 1024, height: 768 },
-                    { width: 1280, height: 1024 },
-                    { width: 1920, height: 1080 },
-                ],
-                rebase: undefined
-            });
+        const result = await generate({
+            inline: false, // generates CSS
+            base: ReleasePath.get(),
+            html: content,
+            dimensions: [
+                { width: 320, height: 568 },
+                { width: 360, height: 720 },
+                { width: 480, height: 800 },
+                { width: 1024, height: 768 },
+                { width: 1280, height: 1024 },
+                { width: 1920, height: 1080 },
+            ],
+            rebase: undefined,
+        });
         css = result.css;
     } catch (e) {
         Logger.error(get_error_message(e, file, 'critical'));

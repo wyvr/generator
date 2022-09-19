40b51af575216373a69f75708bd0c2c28cb0af8f
wyvr <wyvr@daviot.at>
2022-09-19 08:55:00 +0200

    :mute: allow minimizing output of action

--- src/action/media.js
+++ src/action/media.js
@@ -3,15 +3,19 @@ import { Plugin } from '../utils/plugin.js';
 import { WorkerController } from '../worker/controller.js';
 import { measure_action } from './helper.js';
 
-export async function media(files) {
+export async function media(files, minimize_output) {
     const name = 'media';
     const media_files = Object.values(files);
 
-    await measure_action(name, async () => {
-        // wrap in plugin
-        const caller = await Plugin.process(name, media_files);
-        await caller(async (files) => {
-            await WorkerController.process_in_workers(WorkerAction.media, files, 10);
-        });
-    });
+    await measure_action(
+        name,
+        async () => {
+            // wrap in plugin
+            const caller = await Plugin.process(name, media_files);
+            await caller(async (files) => {
+                await WorkerController.process_in_workers(WorkerAction.media, files, 10);
+            });
+        },
+        minimize_output
+    );
 }

c7f33897d87d97751906d5b7bdc8f1fe95e17530
wyvr <wyvr@daviot.at>
2022-10-07 20:03:00 +0200

    :bug: fix when no releases are available

--- src/action/clear_releases.js
+++ src/action/clear_releases.js
@@ -2,7 +2,7 @@ import { readdirSync, statSync } from 'fs';
 import { join } from 'path';
 import { FOLDER_RELEASES } from '../constants/folder.js';
 import { Config } from '../utils/config.js';
-import { remove } from '../utils/file.js';
+import { exists, remove } from '../utils/file.js';
 import { Logger } from '../utils/logger.js';
 import { Plugin } from '../utils/plugin.js';
 import { Cwd } from '../vars/cwd.js';
@@ -11,9 +11,12 @@ import { measure_action } from './helper.js';
 export async function clear_releases(ignore_id) {
     const name = 'clear_releases';
     const keep = Config.get('releases.keep', 0);
+    const path = Cwd.get(FOLDER_RELEASES);
 
+    if(!exists(path)) {
+        return;
+    }
     await measure_action(name, async () => {
-        const path = Cwd.get(FOLDER_RELEASES);
         const releases = readdirSync(path).filter((release) => release != ignore_id);
         await Plugin.before(name, releases, keep, ignore_id);
 

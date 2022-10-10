6b6e3a76a0973c83733384f2c50e15f3cbaafbf9
wyvr <wyvr@daviot.at>
2022-10-10 08:31:09 +0200

    :wrench: add options to the static exec command

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,12 @@
-import { get_exec_request, process_exec_request } from "./action/exec.js";
-import { get_error_message } from "./utils/error.js";
-import { Logger } from "./utils/logger.js";
-import { uniq_id } from "./utils/uniq.js";
+import { get_exec_request, process_exec_request } from './action/exec.js';
+import { get_error_message } from './utils/error.js';
+import { Logger } from './utils/logger.js';
+import { uniq_id } from './utils/uniq.js';
 
 /**
  * Execute the given url
- * @param {string} url 
- * @param {object} options 
+ * @param {string} url
+ * @param {object} options
  * @returns Promise
  */
 export async function exec(url, options) {
@@ -14,7 +14,18 @@ export async function exec(url, options) {
         url,
         path: url,
         method: options?.method || 'GET',
-        isNotExec: true // can indicate that the page is called from direct exec or not
+        isNotExec: true, // can indicate that the page is called from direct exec or not
+        httpVersionMajor: 1,
+        httpVersionMinor: 1,
+        httpVersion: '1.1',
+        complete: true,
+        aborted: false,
+        upgrade: false,
+        statusCode: null,
+        statusMessage: null,
+        headers: options?.headers || {},
+        data: options?.data || {},
+        files: options?.files || {},
     };
     const uid = uniq_id();
     try {

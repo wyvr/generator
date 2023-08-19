4ddac846a521eeb18a55089d2617e5b0ed5ed1cb
wyvr <wyvr@daviot.at>
2023-08-19 21:20:44 +0200

    :coffin: remove commants

--- src/command/dev.js
+++ src/command/dev.js
@@ -54,42 +54,6 @@ export async function dev_command(config) {
 
     await package_watcher(packages);
 
-    /*
-    private init() {
-        Logger.block('watch');
-        this.packages = Config.get('packages');
-        if (!this.packages || !Array.isArray(this.packages) || this.packages.length == 0) {
-            throw 'no packages to watch';
-        }
-
-        // create simple static server
-        const pub = new static_server.Server(join(Cwd.get(), 'pub'), {
-            cache: false,
-            serverInfo: `wyvr`,
-        });
-        this.host = 'localhost';
-        server('localhost', this.ports[0], this.IDLE_TEXT, null, async (req, res, uid) => {
-            pub.serve(req, res, async (err) => {
-                if (err) {
-                    await fallback(req, res, uid, err);
-                }
-            });
-        });
-
-        this.connect();
-
-        // watch for file changes
-        file_watcher(this.packages, (changed_files: IWatchFile[]) => {
-            const force_complete_rebuild = !!changed_files.find((file) => {
-                return file.rel_path.indexOf('plugin') > -1;
-            });
-            this.rebuild(force_complete_rebuild, changed_files);
-        });
-
-        Logger.info('watching', this.packages.length, 'packages');
-    }
-    */
-
     return build_id;
 }
 export function is_fast_build(config, build_id) {

ecf90fce5d5d791e3c1c3e7d493e12553ec838be
wyvr <wyvr@daviot.at>
2023-09-13 10:31:23 +0200

    :bug: set cwd on workers

--- src/worker.js
+++ src/worker.js
@@ -3,8 +3,11 @@ import { WorkerStatus } from './struc/worker_status.js';
 import { IsWorker } from './vars/is_worker.js';
 import { send_status } from './worker/communication.js';
 import { process_message } from './worker/process_message.js';
+import { Cwd } from './vars/cwd.js';
 
 IsWorker.set(true);
+Cwd.set(process.cwd());
+
 process.title = `wyvr worker ${process.pid}`;
 
 send_status(WorkerStatus.exists);

a26ddeeb1bd246945e18574d9225cc044b91c0eb
wyvr <wyvr@daviot.at>
2023-09-21 17:39:58 +0200

    :coffin: remove unused imports

--- src/command/build.js
+++ src/command/build.js
@@ -1,14 +1,11 @@
 import { check_env } from '../action/check_env.js';
 import { clear_releases } from '../action/clear_releases.js';
 import { critical } from '../action/critical.js';
-import { get_config_data } from '../action/get_config_data.js';
 import { intial_build } from '../action/initial_build.js';
 import { optimize } from '../action/optimize.js';
 import { publish } from '../action/publish.js';
 import { sitemap } from '../action/sitemap.js';
-import { Config } from '../utils/config.js';
 import { UniqId } from '../vars/uniq_id.js';
-import { WorkerController } from '../worker/controller.js';
 
 export async function build_command(config) {
     await check_env();

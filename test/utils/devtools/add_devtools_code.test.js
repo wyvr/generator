171a59006b624683dbb7f553e4a91d58562c1365
wyvr <wyvr@daviot.at>
2023-09-26 08:26:26 +0200

    :fire: remove obsolete test

--- test/utils/devtools/add_devtools_code.test.js
+++ test/utils/devtools/add_devtools_code.test.js
@@ -6,14 +6,6 @@ import { Env } from '../../../src/vars/env.js';
 import { Config } from '../../../src/utils/config.js';
 
 describe('utils/devtools/add_devtools_code', () => {
-    const original = Config.get();
-
-    beforeEach(() => {
-        Config.replace({ wsport: '3001' });
-    });
-    afterEach(() => {
-        Config.replace(original);
-    });
     after(() => {
         collect_files('./_tests').forEach((file) => {
             remove(file);
@@ -37,14 +29,6 @@ describe('utils/devtools/add_devtools_code', () => {
         strictEqual(result.indexOf('wyvr_fetch') > -1, true, 'devtools code is missing');
         strictEqual(result.indexOf('wyvr_server_communication') > -1, true, 'client socket code is missing');
         strictEqual(result.indexOf('wyvridentifier') > -1, true, 'identifier is missing');
-    });
-    it('dev mode without client socket code', () => {
-        Config.replace(original);
-        Env.set(EnvType.dev);
-        const result = add_devtools_code('./_tests/index.html', {});
-        Env.value = undefined;
-        strictEqual(result.indexOf('wyvr_fetch') > -1, true, 'devtools code is missing');
-        strictEqual(result.indexOf('wyvr_server_communication') > -1, false, 'client socket code was added');
     });
     it('dev mode for invalid paths', () => {
         Env.set(EnvType.dev);

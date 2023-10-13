88f9a48fbc06b3aa02f99ec43f5cdadc5fc68c56
wyvr <wyvr@daviot.at>
2023-10-13 20:35:54 +0200

    :label: add injectConfig type hints

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Indicates whether the current execution is made on the server
  * Magic value which which are replaced when the content is built
  * @type {boolean}
-*/
+ */
 export const isServer = true;
 
 /**
@@ -13,8 +13,8 @@ export const isServer = true;
 export const isClient = false;
 
 /**
- * Callback which gets executed, this callback can be asynchronous 
- * 
+ * Callback which gets executed, this callback can be asynchronous
+ *
  * @callback onServerCallback
  * @returns void
  * @example
@@ -23,15 +23,15 @@ export const isClient = false;
 
 /**
  * Function which gets only executed when component gets rendered on the server
- * 
+ *
  * @param {onServerCallback} callback Executed function on the server
  * @returns void
  */
 export async function onServer() {}
 
 /**
- * Transform the value with this callback, this callback can be asynchronous 
- * 
+ * Transform the value with this callback, this callback can be asynchronous
+ *
  * @callback injectCallback
  * @param {any} [value=undefined] value of the key
  * @returns any the new value which gets injected
@@ -41,7 +41,7 @@ export async function onServer() {}
 
 /**
  * Inject the value of the key into the component directly
- * 
+ *
  * @param {string} key path to the desired value or object, dot seperated, the first part is the type config, collection, ...
  * @param {any} [fallback=undefined] fallback value if the required key is not found
  * @param {injectCallback} callback Executed before the value gets injected
@@ -52,3 +52,17 @@ export async function onServer() {}
  * _inject('collection.all', [])
  */
 export async function _inject() {}
+
+/**
+ * Inject the value of the key into any source file that gets transformed(contained in packages)
+ * The value gets inserted while transforming into the source file
+ *
+ * @param {string} segment path to the desired value or object in the wyvr config
+ * @param {any} fallback if the path is not found the fallback value gets inserted instead, if not defined, "undefined" is injected
+ * @returns {any}
+ * @example
+ * injectConfig('url')
+ * @example
+ * injectConfig('i18n.fallback', 'en')
+ */
+export function injectConfig() {}

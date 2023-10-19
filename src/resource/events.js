ff63780e39a0fed5ebf82169ba71a6cb829ec17c
wyvr <wyvr@daviot.at>
2023-10-19 21:02:01 +0200

    :bug: avoid multiple setting the events

--- src/resource/events.js
+++ src/resource/events.js
@@ -1,46 +1,51 @@
-// @see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
-(function () {
-    if (typeof window.CustomEvent === 'function') return false;
+if (!window.bind_events) {
+    window.bind_events = true;
 
-    function CustomEvent(event, params) {
-        params = params || { bubbles: false, cancelable: false, detail: null };
-        var evt = document.createEvent('CustomEvent');
-        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-        return evt;
-    }
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+    (function () {
+        if (typeof window.CustomEvent === 'function') return false;
 
-    window.CustomEvent = CustomEvent;
-})();
+        function CustomEvent(event, params) {
+            params = params || { bubbles: false, cancelable: false, detail: null };
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
 
-window.trigger = (event_name, data) => {
-    if (!event_name) {
-        return;
-    }
-    const params = {
-        detail: null,
+        window.CustomEvent = CustomEvent;
+    })();
+    window.trigger = (event_name, data) => {
+        if (!event_name) {
+            return;
+        }
+        const params = {
+            detail: null,
+        };
+
+        if (data) {
+            params.detail = data;
+        }
+        const evt = new CustomEvent(event_name, params);
+        document.dispatchEvent(evt);
     };
-
-    if (data) {
-        params.detail = data;
+    if (!window.on) {
+        window.on = (event_name, callback) => {
+            if (!event_name || !callback) {
+                return;
+            }
+            document.addEventListener(event_name, (e) => {
+                const data = e && e.detail ? e.detail : null;
+                callback(data);
+            });
+        };
     }
-    const evt = new CustomEvent(event_name, params);
-    document.dispatchEvent(evt);
-};
-window.on = (event_name, callback) => {
-    if (!event_name || !callback) {
-        return;
-    }
-    document.addEventListener(event_name, (e) => {
-        const data = e && e.detail ? e.detail : null;
-        callback(data);
-    });
-};
-window.off = (event_name, callback) => {
-    if (!event_name) {
-        return;
-    }
-    document.removeEventListener(event_name, (e) => {
-        const data = e && e.detail ? e.detail : null;
-        callback(data);
-    });
-};
+    window.off = (event_name, callback) => {
+        if (!event_name) {
+            return;
+        }
+        document.removeEventListener(event_name, (e) => {
+            const data = e && e.detail ? e.detail : null;
+            callback(data);
+        });
+    };
+}

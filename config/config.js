52166e1055bb03f898b02685824bd15423aac6d0
wyvr <wyvr@daviot.at>
2023-10-29 20:27:57 +0100

    :coffin: remove old properties

--- config/config.js
+++ config/config.js
@@ -10,10 +10,6 @@ module.exports = {
     releases: {
         keep: 0,
     },
-    import: {
-        format_processed_file: true, // boolean: whether the output of the imported and processed file should be prettyprinted or not
-        measure_performance: true, // boolean: whether the import process should be performance measured
-    },
     packages: null, // list of the packages which should be used
     assets: [], // list of objects { src: 'path_to_source', target: 'relative_path_in_assets' }, to copy files into the assets folder
     default_values: {}, // object which gets injected into every page

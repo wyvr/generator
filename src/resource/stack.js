66649199ca06f75645e971292bfe4622f0709dec
wyvr <wyvr@daviot.at>
2023-11-17 04:44:18 +0100

    :sparkles: add fallback to getStack

--- src/resource/stack.js
+++ src/resource/stack.js
@@ -1,7 +1,8 @@
 /**
  * getStack function retrieves the value of a specified key from the '_stack' object in the global window object.
  *
- * @param {string} key - The key of the value to be retrieved from the '_stack' object.
+ * @param {string} key The key of the value to be retrieved from the '_stack' object.
+ * @param {string} [fallback] The fallback when the key was not found
  * 
  * @return {any} Returns the value associated with the given key if it exists in '_stack'. Otherwise, returns undefined.
  * If the input parameter is not a non-empty string or if the '_stack' object doesn't exist, the function also returns undefined.
@@ -13,10 +14,15 @@
  * @example
  * // If no _stack object is present or key does not exist in _stack
  * console.log(getStack('baz'));  // Logs: undefined
+ * 
+ * @example
+ * // Assuming window._stack = { foo: 'bar' }
+ * console.log(getStack('test', 'fallback'));  // Logs: 'fallback'
+ *
  */
-window.getStack = (key) => {
-    if (typeof key != 'string' || !key || !window._stack) {
-        return undefined;
+window.getStack = (key, fallback) => {
+    if (typeof key == 'string' && key && window._stack) {
+        return window._stack[key] ?? fallback;
     }
-    return window._stack[key];
+    return fallback;
 };

b85d3c3c1ef6a39ed96bf424f3179108f9134bf5
wyvr <wyvr@daviot.at>
2023-12-01 23:19:44 +0100

    :art: clean test

--- test/utils/transform/extract_props.test.js
+++ test/utils/transform/extract_props.test.js
@@ -1,12 +1,8 @@
 import { deepStrictEqual } from 'assert';
 import { describe, it } from 'mocha';
-import { join } from 'path';
-import { to_dirname } from '../../../src/utils/to.js';
 import { extract_props } from '../../../src/utils/transform.js';
 
 describe('utils/transform/extract_props', () => {
-    const __dirname = join(to_dirname(import.meta.url), '_tests', 'combine_splits');
-
     it('undefined', async () => {
         deepStrictEqual(extract_props(), []);
     });

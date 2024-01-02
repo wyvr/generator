0af3144b488699706b4047dcae95916bde2434c3
wyvr <info@wyvr.dev>
2024-01-02 22:41:11 +0100

    :art: format file

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,7 +1,13 @@
 import { createServer } from 'http';
 import formidable, { errors as formidableErrors } from 'formidable';
 import { Logger } from './logger.js';
-import { filled_string, in_array, is_func, is_number } from './validate.js';
+import {
+    filled_string,
+    in_array,
+    is_func,
+    is_number,
+    match_interface,
+} from './validate.js';
 import { uniq_id } from './uniq.js';
 import ServeStatic from 'serve-static';
 import { WebSocketServer } from 'ws';
@@ -9,7 +15,11 @@ import { Cwd } from '../vars/cwd.js';
 import { get_error_message } from './error.js';
 import { nano_to_milli } from './convert.js';
 import { Env } from '../vars/env.js';
-import { route_request, fallback_route_request, apply_response } from '../action/route.js';
+import {
+    route_request,
+    fallback_route_request,
+    apply_response,
+} from '../action/route.js';
 import { config_from_url, get_buffer } from './media.js';
 import { Event } from './event.js';
 import { stringify } from './json.js';
@@ -17,7 +27,7 @@ import { LogType } from '../struc/log.js';
 import { watcher_event } from './watcher.js';
 import { wait_for } from './wait.js';
 import { WatcherPaths } from '../vars/watcher_paths.js';
-import { exists, read } from './file.js';
+import { copy, exists, read, remove } from './file.js';
 import { WorkerController } from '../worker/controller.js';
 import { WorkerAction } from '../struc/worker_action.js';
 import { tmpdir } from 'os';
@@ -36,7 +46,9 @@ let show_requests = true;
 
 export function server(host, port, on_request, on_end) {
     if (!filled_string(host) || !is_number(port)) {
-        Logger.warning('server could not be started because host or port is missing');
+        Logger.warning(
+            'server could not be started because host or port is missing'
+        );
     }
     createServer(async (req, res) => {
         const start = process.hrtime.bigint();
@@ -70,7 +82,9 @@ export function server(host, port, on_request, on_end) {
         const query = {};
         // add query parameters to the request
         if (question_mark > -1) {
-            const params = new URLSearchParams(url.substring(question_mark + 1));
+            const params = new URLSearchParams(
+                url.substring(question_mark + 1)
+            );
             Array.from(params.keys()).forEach((key) => {
                 query[key] = params.get(key);
             });
@@ -84,7 +98,12 @@ export function server(host, port, on_request, on_end) {
             return await final(on_end, req, res, uid, start);
         }
         // allow sending files
-        const form = formidable({ keepExtensions: true, uploadDir: tmpdir(), allowEmptyFiles: true, minFileSize: 0 });
+        const form = formidable({
+            keepExtensions: true,
+            uploadDir: tmpdir(),
+            allowEmptyFiles: true,
+            minFileSize: 0,
+        });
         try {
             [body, files] = await form.parse(req);
             req.body = body;
@@ -98,7 +117,9 @@ export function server(host, port, on_request, on_end) {
     }).listen(port, host, () => {
         const pre_text = 'server started at';
         const text = `http://${host}:${port}`;
-        const filler = new Array(2 + pre_text.length + 1 + text.length).fill('─').join('');
+        const filler = new Array(2 + pre_text.length + 1 + text.length)
+            .fill('─')
+            .join('');
         Logger.output(undefined, undefined, Logger.color.dim(filler));
         Logger.success(pre_text, text);
         Logger.output(undefined, undefined, Logger.color.dim(filler));
@@ -125,7 +146,11 @@ export function log_start(req, uid) {
     return process.hrtime.bigint();
 }
 export function log_end(req, res, uid, start) {
-    const message = [req.method, req.url, ...get_done_log_infos(res.statusMessage, res.statusCode, start, uid)];
+    const message = [
+        req.method,
+        req.url,
+        ...get_done_log_infos(res.statusMessage, res.statusCode, start, uid),
+    ];
     const type = (res.statusCode + '')[0]; // first digit indicates the type
     const type_logger = {
         1: (message) => Logger.info(...message),
@@ -140,14 +165,26 @@ export function is_data_method(method) {
     return ['post', 'patch', 'put'].indexOf(method.toLowerCase()) > -1;
 }
 export async function return_not_found(req, res, uid, message, status, start) {
-    Logger.error(req.method, req.url, ...get_done_log_infos(message, status, start, uid));
+    Logger.error(
+        req.method,
+        req.url,
+        ...get_done_log_infos(message, status, start, uid)
+    );
     if (Env.is_dev()) {
         // use full page in dev mode to add devtools, which allow autoreloading the page
-        const content = read(join(to_dirname(import.meta.url), '..', 'resource', '404development.html'));
+        const content = read(
+            join(
+                to_dirname(import.meta.url),
+                '..',
+                'resource',
+                '404development.html'
+            )
+        );
         if (content) {
             const url = req.url;
             try {
                 const result = await inject(
+                    uid,
                     {
                         result: {
                             html: content
@@ -161,7 +198,10 @@ export async function return_not_found(req, res, uid, message, status, start) {
                     url,
                     '404development'
                 );
-                message = result.content.replace(/<\/body>/, `<script src="/js/404development.js"></script></body>`);
+                message = result.content.replace(
+                    /<\/body>/,
+                    `<script src="/js/404development.js"></script></body>`
+                );
             } catch (e) {
                 Logger.error(get_error_message(e, url, 'return_not_found'));
             }
@@ -172,12 +212,18 @@ export async function return_not_found(req, res, uid, message, status, start) {
     return;
 }
 export function get_done_log_infos(message, status, start, uid) {
-    return [`${status}${Logger.color.dim(`(${message})`)}`, ...get_time_log_infos(start, uid)];
+    return [
+        `${status}${Logger.color.dim(`(${message})`)}`,
+        ...get_time_log_infos(start, uid),
+    ];
 }
 export function get_time_log_infos(start = undefined, uid = undefined) {
     const date = new Date();
     return [
-        start ? nano_to_milli(process.hrtime.bigint() - start) + Logger.color.dim('ms') : undefined,
+        start
+            ? nano_to_milli(process.hrtime.bigint() - start) +
+              Logger.color.dim('ms')
+            : undefined,
         date.toLocaleTimeString(),
         Logger.color.dim(date.toLocaleDateString()),
         uid ? Logger.color.dim(uid) : undefined,
@@ -218,10 +264,24 @@ export async function static_server(req, res, uid, on_end) {
     // static server is not able to handle other requests then get, avoid post, put, patch
     if (!is_data_method(req.method)) {
         static_server_instance(req, res, async () => {
-            await static_server_final({ message: 'Not found', status: 404 }, req, res, uid, on_end, start);
+            await static_server_final(
+                { message: 'Not found', status: 404 },
+                req,
+                res,
+                uid,
+                on_end,
+                start
+            );
         });
     } else {
-        await static_server_final({ message: 'Not found', status: 404 }, req, res, uid, on_end, start);
+        await static_server_final(
+            { message: 'Not found', status: 404 },
+            req,
+            res,
+            uid,
+            on_end,
+            start
+        );
     }
 }
 
@@ -230,7 +290,14 @@ async function static_server_final(err, req, res, uid, on_end, start) {
         await on_end(err, req, res, uid);
 
         if (!res.writableEnded) {
-            return await return_not_found(req, res, uid, err.message, err.status, start);
+            return await return_not_found(
+                req,
+                res,
+                uid,
+                err.message,
+                err.status,
+                start
+            );
         }
     }
     if (res.writableEnded) {
@@ -242,7 +309,9 @@ async function static_server_final(err, req, res, uid, on_end, start) {
 export async function app_server(host, port) {
     return new Promise((resolve, reject) => {
         generate_server(host, port, false, undefined, undefined).catch((e) => {
-            Logger.error(get_error_message(e, undefined, 'worker error ' + process.pid));
+            Logger.error(
+                get_error_message(e, undefined, 'worker error ' + process.pid)
+            );
             reject();
         });
     });
@@ -263,7 +332,13 @@ export function watch_server(host, port, wsport, packages, fallback) {
     websocket_server(wsport, packages);
 }
 
-async function generate_server(host, port, force_generating_of_resources, onEnd, fallback) {
+async function generate_server(
+    host,
+    port,
+    force_generating_of_resources,
+    onEnd,
+    fallback
+) {
     register_stack();
     server(host, port, undefined, async (req, res, uid) => {
         // check if pub is available
@@ -280,14 +355,20 @@ async function generate_server(host, port, force_generating_of_resources, onEnd,
                 if (IsWorker.get()) {
                     await media([media_config]);
                 } else {
-                    await WorkerController.process_data(WorkerAction.media, [media_config]);
+                    await WorkerController.process_data(WorkerAction.media, [
+                        media_config,
+                    ]);
                 }
                 // the file needs some time to be available after generation started
                 const success = await wait_for(() => {
                     return exists(Cwd.get(media_config.result));
                 });
                 if (show_requests) {
-                    Logger[success ? 'success' : 'error']('media', name, ...get_time_log_infos(start, uid));
+                    Logger[success ? 'success' : 'error'](
+                        'media',
+                        name,
+                        ...get_time_log_infos(start, uid)
+                    );
                 }
             }
         }
@@ -295,7 +376,9 @@ async function generate_server(host, port, force_generating_of_resources, onEnd,
             if (err) {
                 // check if css or js files should be generated
                 if (req.url.indexOf('/js') == 0) {
-                    const file_content = await build_hydrate_file_from_url(req.url);
+                    const file_content = await build_hydrate_file_from_url(
+                        req.url
+                    );
                     if (file_content) {
                         res = send_head(res, 200, 'application/javascript');
                         res.end(file_content);
@@ -303,7 +386,12 @@ async function generate_server(host, port, force_generating_of_resources, onEnd,
                     }
                 }
 
-                const route_response = await route_request(req, res, uid, force_generating_of_resources);
+                const route_response = await route_request(
+                    req,
+                    res,
+                    uid,
+                    force_generating_of_resources
+                );
                 if (route_response) {
                     res = apply_response(res, route_response);
                     return;
@@ -314,7 +402,8 @@ async function generate_server(host, port, force_generating_of_resources, onEnd,
                         return false;
                     }
                     // fallback_route_request returns an response
-                    const fallback_route_response = await fallback_route_request(req, res, uid);
+                    const fallback_route_response =
+                        await fallback_route_request(req, res, uid);
                     if (fallback_route_response) {
                         res = apply_response(res, fallback_route_response);
                         return;
@@ -363,7 +452,9 @@ export function websocket_server(port, packages) {
                 try {
                     data = JSON.parse(message.toString('utf8'));
                 } catch (e) {
-                    Logger.warning(get_error_message(e, undefined, 'websocket'));
+                    Logger.warning(
+                        get_error_message(e, undefined, 'websocket')
+                    );
                 }
             }
             Logger.debug('client message', data);
@@ -378,10 +469,16 @@ export function websocket_server(port, packages) {
                             let path;
                             Logger.block('rebuild', data.data);
                             // check if the url is a route
-                            const route = get_route(data.data, 'get', get_config_cache('route.cache'));
+                            const route = get_route(
+                                data.data,
+                                'get',
+                                get_config_cache('route.cache')
+                            );
                             if (route) {
                                 const found_route = packages
-                                    .map((pkg) => join(pkg.path, route.rel_path))
+                                    .map((pkg) =>
+                                        join(pkg.path, route.rel_path)
+                                    )
                                     .find((file) => exists(file));
                                 if (found_route) {
                                     path = found_route;

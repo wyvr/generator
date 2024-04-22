9597bd6f94a1bcd999852fbf86716422c3a4cb0e
wyvr <info@wyvr.dev>
2024-04-22 21:34:46 +0200

    bug: fix mocha compatibility

--- src/utils/tests.js
+++ src/utils/tests.js
@@ -5,28 +5,22 @@ import { filled_array, filled_string, is_object } from './validate.js';
 import { uniq_values } from './uniq.js';
 const { EVENT_TEST_FAIL } = Mocha.Runner.constants; // other constants https://mochajs.org/api/runner.js.html
 
+let stats;
 export async function run_tests(files) {
     if (!filled_array(files)) {
         return;
     }
+    stats = undefined;
     /* c8 ignore start */
     return await new Promise((resolve) => {
-        let stats = null;
         const mocha = new Mocha({
-            reporter: function (runner) {
-                stats = runner.stats;
-                let number = 1;
-                runner.on(EVENT_TEST_FAIL, (test, err) => {
-                    Logger.error([`failed test #${number}`, Logger.color.bold(test.fullTitle()), Logger.color.dim(`file:${test.file}`), err.message, ''].join('\n'));
-                    number++;
-                });
-            }
+            reporter: WyvrReporter
         });
-        uniq_values(files).forEach((file) => {
+        for (const file of uniq_values(files)) {
             if (filled_string(file)) {
                 mocha.addFile(file);
             }
-        });
+        }
         mocha
             .loadFilesAsync({
                 // needed to avoid caching of resources
@@ -35,11 +29,11 @@ export async function run_tests(files) {
                 }
             })
             .then(() => {
-                mocha.run(function (failures) {
-                    const result = { ok: failures == 0, stats, failures, files };
+                mocha.run((failures) => {
+                    const result = { ok: failures === 0, stats, failures, files };
                     if (!failures) {
                         const amount = stats?.tests || 0;
-                        Logger.success(`${amount} test${amount == 1 ? '' : 's'}`, 'run successfully');
+                        Logger.success(`${amount} test${amount === 1 ? '' : 's'}`, 'run successfully');
                     } else {
                         Logger.error('failed tests', failures);
                     }
@@ -48,4 +42,15 @@ export async function run_tests(files) {
             });
     });
     /* c8 ignore end */
+}
+
+class WyvrReporter {
+    constructor(runner) {
+        stats = runner.stats;
+        let number = 1;
+        runner.on(EVENT_TEST_FAIL, (test, err) => {
+            Logger.error([`failed test #${number}`, Logger.color.bold(test.fullTitle()), Logger.color.dim(`file:${test.file}`), err.message, ''].join('\n'));
+            number++;
+        });
+    }
 }

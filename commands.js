a91e45e9e6f0f4896c4166c6afeeea1dce1cefab
wyvr <info@wyvr.dev>
2024-05-18 00:17:38 +0200

    feat: add commands helper and extract from the cron helper into shared

---
+++ commands.js
@@ -0,0 +1,110 @@
+import { get_error_message } from './src/utils/error.js';
+import { Logger } from './src/utils/logger.js';
+import { collect_data_from_cli } from './src/cli/interactive.js';
+import { filled_array, is_func } from './src/utils/validate.js';
+
+export * from './shared.js';
+
+const prompts = [];
+export function prompt_input(name, message, default_value, validate) {
+    const data = prompt(name, message, default_value, validate);
+    data.type = 'input';
+    return data;
+}
+
+export function prompt_number(name, message, default_value, validate) {
+    const data = prompt(name, message, default_value, validate);
+    data.type = 'number';
+    return data;
+}
+
+export function prompt_confirm(name, message, default_value, validate) {
+    const data = prompt(name, message, !!default_value, validate);
+    data.type = 'confirm';
+    return data;
+}
+
+export function prompt_list(name, message, choices, validate) {
+    const data = prompt(name, message, 0, validate);
+    data.type = 'list';
+    if (filled_array(choices)) {
+        data.choices = choices;
+    }
+    return data;
+}
+
+export function prompt_checkbox(name, message, choices, validate) {
+    const data = prompt(name, message, 0, validate);
+    data.type = 'checkbox';
+    if (filled_array(choices)) {
+        data.choices = choices;
+    }
+    return data;
+}
+
+export function prompt_password(name, message, validate) {
+    const data = prompt(name, message, 0, validate);
+    data.type = 'password';
+    data.mask = false;
+    return data;
+}
+
+export function prompt_choice(name, value, checked = false) {
+    return { name, value, checked };
+}
+
+export function prompt_condition(field, conditions, default_value) {
+    return { _field: field, ...conditions, _: default_value };
+}
+
+function prompt(name, message, default_value, validate) {
+    const data = { name, message };
+    if (default_value) {
+        data.default = default_value;
+    }
+    if (is_func(validate)) {
+        data.validate = validate;
+    }
+    return data;
+}
+
+/**
+ * Adds prompts to the prompts array.
+ * @param {...any} questions - The prompts to be added.
+ */
+export function add_prompts_group(...questions) {
+    prompts.push(questions);
+}
+
+/**
+ * Retrieves the prompts.
+ *
+ * @returns {Array} The prompts array.
+ */
+export function get_prompts() {
+    return prompts;
+}
+
+/**
+ * Clears the prompts array.
+ */
+export function clear_prompts() {
+    prompts.length = 0;
+}
+
+/**
+ * Executes prompts and collects data from the command line interface.
+ *
+ * @param {Object} default_data - The default data to be used for prompts.
+ * @returns {Promise<Object|undefined>} - A promise that resolves to the collected data or undefined if an error occurs.
+ */
+export async function execute_prompts(default_data = {}) {
+    try {
+        const result = await collect_data_from_cli(prompts, default_data);
+        clear_prompts();
+        return result;
+    } catch (e) {
+        Logger.error(get_error_message(e, undefined, 'execute prompts'));
+        return undefined;
+    }
+}
---
+++ shared.js
@@ -0,0 +1,163 @@
+import { get_error_message as gem } from './src/utils/error.js';
+import { Logger } from './src/utils/logger.js';
+import { uniq_id } from './src/utils/uniq.js';
+import { Config } from './src/utils/config.js';
+import { get_page_from_url, update_pages_cache } from './src/utils/pages.js';
+import { regenerate_pages } from './src/utils/regenerate.js';
+import { Cwd } from './src/vars/cwd.js';
+import { FOLDER_GEN } from './src/constants/folder.js';
+import { WorkerController } from './src/worker/controller.js';
+import { filled_array, filled_string } from './src/utils/validate.js';
+import { WorkerAction } from './src/struc/worker_action.js';
+import { remove_index, read_json, write_json } from './src/utils/file.js';
+import { Env } from './src/vars/env.js';
+import { get_route_request } from './src/utils/routes.js';
+import { process_route_request } from './src/action_worker/route.js';
+import { register_stack } from './src/utils/global.js';
+import { get_config_cache_path } from './src/utils/config_cache.js';
+import { join } from 'node:path';
+
+
+if (typeof window !== 'undefined' || typeof process === 'undefined') {
+    throw new Error('This module is only allowed in server runtimes like Node.js, if you see this error please report it to the library author.');
+}
+
+register_stack();
+
+/**
+ * Execute the route for the given url
+ * This can cause multiple pages to be regenerated at once
+ *
+ * @param {string} url
+ * @param {object} options
+ * @returns {Promise}
+ */
+export async function execute_route(url, options) {
+    if (Env.is_dev()) {
+        Logger.warning('route', url, 'will not be generated in dev mode');
+        return false;
+    }
+    const request = {
+        url: remove_index(url),
+        path: url,
+        method: options?.method || 'GET',
+        isNotExec: true, // can indicate that the page is called from direct exec or not
+        httpVersionMajor: 1,
+        httpVersionMinor: 1,
+        httpVersion: '1.1',
+        complete: true,
+        aborted: false,
+        upgrade: false,
+        statusCode: null,
+        statusMessage: null,
+        headers: options?.headers || {},
+        data: options?.data || {},
+        body: options?.body || {},
+        files: options?.files || {}
+    };
+    const uid = uniq_id();
+    try {
+        const exec = get_route_request(request);
+        if (exec) {
+            Logger.debug('exec', url, exec.url);
+            return await process_route_request(request, undefined, uid, exec, false);
+        }
+    } catch (e) {
+        Logger.error(gem(e, url, 'cron route'));
+    }
+    return false;
+}
+
+/**
+ * Execute the page for the given url
+ * This can cause multiple pages to be regenerated at once
+ * @param {string} url
+ * @returns {Promise}
+ */
+export async function execute_page(url) {
+    const page = get_page_from_url(url);
+
+    if (!page) {
+        return undefined;
+    }
+    try {
+        const { pages, page_objects } = await regenerate_pages({ change: [page] }, {}, [], Cwd.get(FOLDER_GEN));
+        if (!filled_array(pages)) {
+            Logger.error('no pages were regenerated');
+            return false;
+        }
+        update_pages_cache(page_objects);
+        await WorkerController.process_in_workers(WorkerAction.build, pages, 100, true);
+
+        Logger.improve('persisted', page_objects.map((page) => page.urls.join(' ')).join(' '));
+        return page_objects;
+    } catch (e) {
+        Logger.error(get_error_message(e, url, 'cron page'));
+    }
+    return false;
+}
+
+/**
+ * Get config value
+ * @param {string} segment
+ * @param {any} fallback_value
+ * @returns {any}
+ */
+export function get_config(segment, fallback_value) {
+    return Config.get(segment, fallback_value);
+}
+
+/**
+ * Get logger instance
+ * @deprecated Use logger from @wyvr/generator/universal.js instead
+ * @returns {Logger}
+ */
+export function get_logger() {
+    Logger.warning('get_logger from cron.js is deprecated and will be removed in the future, please use logger from @wyvr/generator/universal.js instead.');
+    return Logger;
+}
+
+export const get_error_message = gem;
+
+/**
+ * Retrieves the cache data for the specified cache name and scope.
+ * @param {string} cache_name - The name of the cache.
+ * @param {string} [scope='default'] - The scope of the cache. Defaults to 'default'.
+ * @returns {object} - The cache data as a JavaScript object.
+ */
+export function get_cache(cache_name, scope = 'default') {
+    return read_json(get_cache_path(cache_name, scope));
+}
+
+/**
+ * Sets the cache with the specified name and value.
+ * @param {string} cache_name - The name of the cache.
+ * @param {any} value - The value to be stored in the cache.
+ * @param {string} [scope='default'] - The scope of the cache (optional, default is 'default').
+ * @returns {boolean} - Returns true if the cache is successfully set, false otherwise.
+ */
+export function set_cache(cache_name, value, scope = 'default') {
+    if (!filled_string(cache_name)) {
+        return false;
+    }
+    return write_json(get_cache_path(cache_name, scope), value);
+}
+
+/**
+ * Returns the cache path for a given cache name and scope.
+ * @param {string} cache_name - The name of the cache.
+ * @param {string} [scope='default'] - The scope of the cache. Defaults to 'default'.
+ * @returns {string} The cache path.
+ */
+export function get_cache_path(cache_name, scope = 'default') {
+    const cache_key = join(
+        scope,
+        cache_name
+            .replace(/^\//, '')
+            .replace(/\/$/, '')
+            .replace(/\//g, '|')
+            .replace(/[^\w|/]/g, '-')
+    );
+
+    return get_config_cache_path(cache_key);
+}

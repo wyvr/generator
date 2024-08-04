1b7f7640357d6be0dc7728591f33fec2c95f7227
wyvr <info@wyvr.dev>
2024-08-04 21:48:59 +0200

    feat: remove bid from the resources

--- src/boilerplate/src/wyvr/Script.svelte
+++ src/boilerplate/src/wyvr/Script.svelte
@@ -3,21 +3,12 @@
         import Script from '$src/wyvr/Script.svelte';
         
         <Script src="/assets/script.css" /> 
-        */
-    import { append_query_string } from '$src/wyvr/append_query_string.js';
+    */
 
     export let src = null;
     export let defer = true;
-    const build_id = _inject('config.build_id', undefined, (value) => {
-        if (value) {
-            return value.substr(0, 8);
-        }
-        return value;
-    });
-
-    $: href = append_query_string(src, 'bid', build_id);
 </script>
 
-{#if href}
-    <script {defer} src={href}></script>
+{#if src}
+    <script {defer} {src}></script>
 {/if}
--- src/boilerplate/src/wyvr/Stylesheet.svelte
+++ src/boilerplate/src/wyvr/Stylesheet.svelte
@@ -4,26 +4,23 @@
 
         <Stylesheet src="/assets/global.css" /> 
     */
-    import { append_query_string } from '$src/wyvr/append_query_string.js';
 
     export let src = null;
     export let critical = false;
     export let media;
-    const build_id = _inject('config.build_id', undefined, (value) => {
-        if (value) {
-            return value.substr(0, 8);
-        }
-        return value;
-    });
-
-    $: href = append_query_string(src, 'bid', build_id);
 </script>
 
-{#if href}
+{#if src}
     {#if critical}
-        <link rel="stylesheet" {href} {media} />
+        <link rel="stylesheet" href={src} {media} />
     {:else}
-        <link rel="preload" {href} as="style" onload="this.onload=null;this.rel='stylesheet'" {media} />
-        <noscript><link rel="stylesheet" {href} {media} /></noscript>
+        <link
+            rel="preload"
+            href={src}
+            as="style"
+            onload="this.onload=null;this.rel='stylesheet'"
+            {media}
+        />
+        <noscript><link rel="stylesheet" href={src} {media} /></noscript>
     {/if}
 {/if}

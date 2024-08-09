188867d0366d483c637c712f21e82556e2f1ecb5
wyvr <info@wyvr.dev>
2024-08-10 00:24:51 +0200

    fix: reenable optimiztion of html pages

--- src/action_worker/optimize.js
+++ src/action_worker/optimize.js
@@ -5,13 +5,10 @@ import { extname } from 'node:path';
 
 import { get_error_message } from '../utils/error.js';
 import { ReleasePath } from '../vars/release_path.js';
-import { KeyValue } from '../utils/database/key_value.js';
-import { STORAGE_OPTIMIZE_CRITICAL } from '../constants/storage.js';
 import { optimize_content } from '../utils/optimize.js';
 import { optimize_css } from '../utils/optimize/css.js';
 import { optimize_js } from '../utils/optimize/js.js';
 
-const critical_db = new KeyValue(STORAGE_OPTIMIZE_CRITICAL);
 export async function optimize(files) {
     if (!filled_array(files)) {
         return false;
@@ -27,6 +24,12 @@ export async function optimize(files) {
             const rel_path = file.replace(ReleasePath.get(), '');
 
             switch (extname(file)) {
+                case '.html':
+                case '.htm': {
+                    const optimized_content = await optimize_content(content);
+                    write(file, optimized_content);
+                    break;
+                }
                 case '.css': {
                     await optimize_css(content, rel_path);
                     break;

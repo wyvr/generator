15ba1eb5b5815dd9c0d9a623844242c599a38b36
wyvr <info@wyvr.dev>
2024-08-12 14:51:47 +0200

    fix: add timestamp in dev mode again

--- src/boilerplate/src/wyvr/Script.svelte
+++ src/boilerplate/src/wyvr/Script.svelte
@@ -1,14 +1,20 @@
 <script>
-/* 
+    /* 
         import Script from '$src/wyvr/Script.svelte';
         
         <Script src="/assets/script.css" /> 
     */
 
-export let src = null;
-export let defer = true;
+    import { append_query_string } from '$src/wyvr/append_query_string.js';
+
+    export let src = null;
+    export let defer = true;
+
+    const timestamp = _inject('config.env') === 'dev' ? Date.now() : undefined;
+
+    $: source = src ? append_query_string(src, 'ts', timestamp) : undefined;
 </script>
 
-{#if src}
-    <script {defer} {src}></script>
+{#if source}
+    <script {defer} src={source}></script>
 {/if}
--- src/boilerplate/src/wyvr/Stylesheet.svelte
+++ src/boilerplate/src/wyvr/Stylesheet.svelte
@@ -1,26 +1,35 @@
 <script>
-/* 
+    /* 
         import Stylesheet from '$src/wyvr/Stylesheet.svelte';
 
         <Stylesheet src="/assets/global.css" /> 
     */
+    import { append_query_string } from '$src/wyvr/append_query_string.js';
 
-export let src = null;
-export let critical = false;
-export let media;
+    export let src = null;
+    export let href = null;
+    export let critical = false;
+    export let media;
+
+    const timestamp = _inject('config.env') === 'dev' ? Date.now() : undefined;
+
+    $: raw_source = href ?? src;
+    $: source = raw_source
+        ? append_query_string(raw_source, 'ts', timestamp)
+        : undefined;
 </script>
 
-{#if src}
+{#if source}
     {#if critical}
-        <link rel="stylesheet" href={src} {media} />
+        <link rel="stylesheet" href={source} {media} />
     {:else}
         <link
             rel="preload"
-            href={src}
+            href={source}
             as="style"
             onload="this.onload=null;this.rel='stylesheet'"
             {media}
         />
-        <noscript><link rel="stylesheet" href={src} {media} /></noscript>
+        <noscript><link rel="stylesheet" {href} {media} /></noscript>
     {/if}
 {/if}

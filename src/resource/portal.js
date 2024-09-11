f7d547fd9ff9b39c4de4fd8b05f23744a44295a8
wyvr <info@wyvr.dev>
2024-09-11 09:40:29 +0200

    refactor: improve code

--- src/resource/portal.js
+++ src/resource/portal.js
@@ -6,24 +6,26 @@ export function wyvr_portal(el, props) {
     }
     const portal_prop = el.getAttribute('data-portal');
     if (portal_prop) {
-        const portal_target_selector = props[portal_prop];
-        if (portal_target_selector) {
-            const portal_target = document.querySelector(portal_target_selector);
-            if (portal_target) {
-                // port all attributes to the new element
-                for (const attr of Array.from(el.attributes)) {
-                    if (attr.name !== 'data-portal') {
-                        portal_target.setAttribute(attr.name, attr.value);
-                    }
-                }
-                portal_target.innerHTML = el.innerHTML;
-                el.remove();
-
-                return portal_target;
-            }
+        return el;
+    }
+    const portal_target_selector = props[portal_prop];
+    if (!portal_target_selector) {
+        return el;
+    }
+    const portal_target = document.querySelector(portal_target_selector);
+    if (!portal_target) {
+        return el;
+    }
+    // port all attributes to the new element
+    for (const attr of Array.from(el.attributes)) {
+        if (attr.name !== 'data-portal') {
+            portal_target.setAttribute(attr.name, attr.value);
         }
     }
-    return el;
+    portal_target.innerHTML = el.innerHTML;
+    el.remove();
+
+    return portal_target;
 }
 
 /**

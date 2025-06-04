88f27f708ce76ec58704b2aa04c415ca028377f5
wyvr <info@wyvr.dev>
2025-06-04 10:08:23 +0200

    feat: remove gm package and the support of png gamma correction

--- src/utils/media.js
+++ src/utils/media.js
@@ -17,7 +17,6 @@ import { Config } from './config.js';
 import { get_config_cache, set_config_cache } from './config_cache.js';
 import { IsWorker } from '../vars/is_worker.js';
 import { to_media_config, to_media_hash } from '../boilerplate/src/wyvr/media.js';
-import gm from 'gm';
 import { PLUGIN_MEDIA_CONFIG } from '../constants/plugins.js';
 
 let cache;
@@ -132,34 +131,12 @@ export async function process(media) {
     if (media.output !== MediaModelOutput.path) {
         Logger.warning('media', `${media.src} output "${media.output}" is not implemented at the moment, falling back to path`);
     }
-    /* @TODO sharp does not provide a method to get the gamma value of an image,
-     * some systems add a "wrong" gamma value to it which makes the images darker,
-     * the solution for this images wolud be `.gamma(1, 2.2)` otherwise `.gamma(2.2, 2.2)`
-     */
+
+    // correct gamma value
     if (media.format === 'png' || media.ext === 'png') {
-        let magick = gm;
-        const image_magick = Config.get('media.image_magick');
-        if (match_interface(image_magick, { version: true })) {
-            const im_config = {
-                imageMagick: '7+'
-            };
-            // legacy mode
-            if (image_magick.version < 7) {
-                im_config.imageMagick = true;
-            }
-            magick = gm.subClass(im_config);
-        }
-        const gamma = await new Promise((resolve) => {
-            magick(buffer, 'image.png').identify(async (err, data) => {
-                // in case of an error or when no gamma value is set on the png ignore it
-                if (err || data?.Gamma !== '1') {
-                    resolve(2.2);
-                }
-                resolve(1);
-            });
-        });
-        modified_image = await modified_image.gamma(gamma, 2.2);
+        modified_image = await correct_gamma_value(modified_image);
     }
+
     let output_buffer;
     switch (media.format) {
         case 'jpg':
@@ -196,6 +173,41 @@ export async function process(media) {
     }
 
     return undefined;
+}
+
+async function correct_gamma_value(modified_image) {
+     /* @TODO sharp does not provide a method to get the gamma value of an image,
+     * some systems add a "wrong" gamma value to it which makes the images darker,
+     * the solution for this images wolud be `.gamma(1, 2.2)` otherwise `.gamma(2.2, 2.2)`
+     */
+    // import gm of 'gm'
+    let magick = typeof global['gm'] === 'function' ? gm : null;
+    if (!magick) {
+        return modified_image;
+    }
+    // can never be reached, it is a edge case and gm is sunsetted so it is not supported
+    const image_magick = Config.get('media.image_magick');
+    if (match_interface(image_magick, { version: true })) {
+        const im_config = {
+            imageMagick: '7+'
+        };
+        // legacy mode
+        if (image_magick.version < 7) {
+            im_config.imageMagick = true;
+        }
+        magick = gm.subClass(im_config);
+    }
+    const gamma = await new Promise((resolve) => {
+        magick(buffer, 'image.png').identify(async (err, data) => {
+            // in case of an error or when no gamma value is set on the png ignore it
+            if (err || data?.Gamma !== '1') {
+                resolve(2.2);
+            }
+            resolve(1);
+        });
+    });
+    modified_image = await modified_image.gamma(gamma, 2.2);
+    return modified_image;
 }
 
 export async function config_from_url(url) {
